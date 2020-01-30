23a919ef81ee321a3a6e115b71fcbe48c534107c
Phuoc Nguyen <me@phuoc.ng>
2020-01-30 19:42:11 +0700

    Add a minified version for umd package

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import css from 'rollup-plugin-css-only';
+import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
 
@@ -35,6 +36,38 @@ export default [
                 resolveJsonModule: true,
                 moduleResolution: 'node',
             }),
+        ],
+    },
+
+    // Minified UMD
+    {
+        input,
+        output: {
+            file: './dist/umd/react-pdf-viewer.min.js',
+            format: 'umd',
+            name: 'ReactPdfViewer',
+            globals: {
+                PdfJs: 'pdfjs-dist',
+                react: 'React',
+                'react-dom': 'ReactDOM',
+            },
+        },
+        external: ['pdfjs-dist', 'react', 'react-dom'],
+        plugins: [
+            json(),
+            css({
+                output: './dist/umd/react-pdf-viewer.css',
+            }),
+            typescript({
+                removeComments: true,
+                module: 'es6',
+                target: 'es5',
+                jsx: 'react',
+                allowSyntheticDefaultImports: true,
+                resolveJsonModule: true,
+                moduleResolution: 'node',
+            }),
+            terser(),
         ],
     },
 
