d0bbc7774cbeb79f7ad61a5d3cb51bae52e728e2
Phuoc Nguyen <me@phuoc.ng>
2020-03-07 06:11:47 +0700

    Move annotation layer styles to less

--- src/layers/AnnotationLayer.tsx
+++ src/layers/AnnotationLayer.tsx
@@ -9,6 +9,7 @@
 import React from 'react';
 
 import PdfJs from '../PdfJs';
+import ThemeContent from '../theme/ThemeContext';
 import getDestination from '../utils/getDestination';
 import WithScale from '../WithScale';
 import { SpecialLevel } from '../zoom/zoomingLevel';
@@ -24,6 +25,7 @@ interface AnnotationLayerProps {
 
 const AnnotationLayer: React.FC<AnnotationLayerProps> = ({ doc, page, rotation, scale, onJumpToDest }) => {
     const containerRef = React.createRef<HTMLDivElement>();
+    const theme = React.useContext(ThemeContent);
 
     const renderAnnotation = () => {
         const container = containerRef.current;
@@ -64,7 +66,7 @@ const AnnotationLayer: React.FC<AnnotationLayerProps> = ({ doc, page, rotation, 
 
     return (
         <WithScale callback={renderAnnotation} rotation={rotation} scale={scale}>
-            <div className="viewer-annotation" ref={containerRef} />
+            <div className={`${theme.prefixClass}-annotation`} ref={containerRef} />
         </WithScale>
     );
 };
