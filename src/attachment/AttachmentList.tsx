2715e6d02687fdb4756fab8e54c3d6c1d168295d
Phuoc Nguyen <me@phuoc.ng>
2020-03-06 22:23:04 +0700

    Move attachment styles to less

--- src/attachment/AttachmentList.tsx
+++ src/attachment/AttachmentList.tsx
@@ -9,6 +9,7 @@
 import React from 'react';
 
 import LocalizationContext from '../localization/LocalizationContext';
+import ThemeContent from '../theme/ThemeContext';
 import downloadFile from '../utils/downloadFile';
 import './attachmentList.less';
 import FileItem from './FileItem';
@@ -19,14 +20,14 @@ interface AttachmentListProps {
 
 const AttachmentList: React.FC<AttachmentListProps> = ({ files }) => {
     const l10n = React.useContext(LocalizationContext);
+    const theme = React.useContext(ThemeContent);
 
     const renderItem = (file: FileItem) => {
         const onClick = () => downloadFile(file.fileName, file.data);
         return (
             <li
-                className="viewer-attachment-item"
+                className={`${theme.prefixClass}-attachment-item`}
                 key={`attachment-${file.fileName}`}
-                style={{ padding: '8px' }}
                 title={`${l10n.attachment.clickToDownload}`}
                 onClick={onClick}
             >
@@ -39,14 +40,7 @@ const AttachmentList: React.FC<AttachmentListProps> = ({ files }) => {
         files.length === 0
             ? <div>{l10n.attachment.noAttachment}</div>
             : (
-                <ul
-                    style={{
-                        listStyleType: 'none',
-                        margin: '0',
-                        padding: '0',
-                        width: '100%',
-                    }}
-                >
+                <ul className={`${theme.prefixClass}-attachment-list`}>
                     {
                         files.map(renderItem)
                     }
