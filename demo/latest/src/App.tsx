3fa856947c8d78dae41a1d1f391942c758c3bbe5
Phuoc Nguyen <me@phuoc.ng>
2020-03-29 18:41:56 +0700

    Add load from base64 example

--- demo/latest/src/App.tsx
+++ demo/latest/src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Worker } from '@phuocng/react-pdf-viewer';
 
 import '@phuocng/react-pdf-viewer/cjs/react-pdf-viewer.css';
-import LoadFromBytesExample from './LoadFromBytesExample';
+import LoadFromBase64Example from './LoadFromBase64Example';
 
 const App = () => {
     return (
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.2.228/build/pdf.worker.min.js">
             <div style={{ height: '750px', padding: '16px 0' }}>
-                <LoadFromBytesExample />
+                <LoadFromBase64Example />
             </div>
         </Worker>
     );
---
+++ demo/latest/src/LoadFromBase64Example.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Viewer from '@phuocng/react-pdf-viewer';
+
+const LoadFromBase64Example: React.FC<{}> = () => {
+    const base64 = 'data:application/pdf;base64,JVBERi0xLjcKCjEgMCBvYmogICUgZW50cnkgcG9pbnQKPDwKICAvVHlwZSAvQ2F0YWxvZwogIC9QYWdlcyAyIDAgUgo+PgplbmRvYmoKCjIgMCBvYmoKPDwKICAvVHlwZSAvUGFnZXMKICAvTWVkaWFCb3ggWyAwIDAgMjAwIDIwMCBdCiAgL0NvdW50IDEKICAvS2lkcyBbIDMgMCBSIF0KPj4KZW5kb2JqCgozIDAgb2JqCjw8CiAgL1R5cGUgL1BhZ2UKICAvUGFyZW50IDIgMCBSCiAgL1Jlc291cmNlcyA8PAogICAgL0ZvbnQgPDwKICAgICAgL0YxIDQgMCBSIAogICAgPj4KICA+PgogIC9Db250ZW50cyA1IDAgUgo+PgplbmRvYmoKCjQgMCBvYmoKPDwKICAvVHlwZSAvRm9udAogIC9TdWJ0eXBlIC9UeXBlMQogIC9CYXNlRm9udCAvVGltZXMtUm9tYW4KPj4KZW5kb2JqCgo1IDAgb2JqICAlIHBhZ2UgY29udGVudAo8PAogIC9MZW5ndGggNDQKPj4Kc3RyZWFtCkJUCjcwIDUwIFRECi9GMSAxMiBUZgooSGVsbG8sIHdvcmxkISkgVGoKRVQKZW5kc3RyZWFtCmVuZG9iagoKeHJlZgowIDYKMDAwMDAwMDAwMCA2NTUzNSBmIAowMDAwMDAwMDEwIDAwMDAwIG4gCjAwMDAwMDAwNzkgMDAwMDAgbiAKMDAwMDAwMDE3MyAwMDAwMCBuIAowMDAwMDAwMzAxIDAwMDAwIG4gCjAwMDAwMDAzODAgMDAwMDAgbiAKdHJhaWxlcgo8PAogIC9TaXplIDYKICAvUm9vdCAxIDAgUgo+PgpzdGFydHhyZWYKNDkyCiUlRU9G';
+    const pdfContentType = 'application/pdf';
+
+    const base64toBlob = (data: string) => {
+        // Cut the prefix `data:application/pdf;base64` from the raw base 64
+        const base64WithoutPrefix = data.substr(`data:${pdfContentType};base64,`.length);
+
+        const bytes = atob(base64WithoutPrefix);
+        let length = bytes.length;
+        let out = new Uint8Array(length);
+    
+        while (length--) {
+            out[length] = bytes.charCodeAt(length);
+        }
+
+        return new Blob([out], { type: pdfContentType });
+    };
+
+    const url = URL.createObjectURL(base64toBlob(base64));
+
+    return (
+        <div style={{ height: '750px', padding: '16px 0' }}>
+            <Viewer fileUrl={url} />
+        </div>
+    );
+};
+
+export default LoadFromBase64Example;
