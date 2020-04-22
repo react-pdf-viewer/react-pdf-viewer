603c5684f2df4adaa678b411353cf82413705ca6
Phuoc Nguyen <me@phuoc.ng>
2020-04-22 10:21:27 +0700

    Add example renders the canvas layer only

---
+++ demo/latest/src/CanvasLayerExample.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Viewer, { RenderPageProps } from '@phuocng/react-pdf-viewer';
+
+interface CanvasLayerExampleProps {
+    fileUrl: string;
+}
+
+const CanvasLayerExample: React.FC<CanvasLayerExampleProps> = ({ fileUrl }) => {
+    const renderPage = (props: RenderPageProps) => {
+        return (
+            <>
+                {props.canvasLayer.children}
+            </>
+        );
+    };
+
+    return (
+        <Viewer
+            fileUrl={fileUrl}
+            renderPage={renderPage}
+            defaultScale={1}
+        />
+    );
+};
+
+export default CanvasLayerExample;
