e96bcf3af64cb7bd706031cd169a262343735ecd
Phuoc Nguyen <me@phuoc.ng>
2020-04-23 20:53:12 +0700

    Update modal structure

--- src/portal/ModalBody.tsx
+++ src/portal/ModalBody.tsx
@@ -8,22 +8,25 @@
 
 import React from 'react';
 
+import useClickOutside from '../hooks/useClickOutside';
 import useKeyUp from '../hooks/useKeyUp';
 import useLockScroll from '../hooks/useLockScroll';
 import ThemeContent from '../theme/ThemeContext';
 import './modalBody.less';
 
 interface ModalBodyProps {
+    closeOnClickOutside: boolean;
     closeOnEscape: boolean;
     onToggle(): void;
 }
 
-const ModalBody: React.FC<ModalBodyProps> = ({ children, closeOnEscape, onToggle }) => {
+const ModalBody: React.FC<ModalBodyProps> = ({ children, closeOnClickOutside, closeOnEscape, onToggle }) => {
     const theme = React.useContext(ThemeContent);
     const contentRef = React.createRef<HTMLDivElement>();
 
     useLockScroll();
     useKeyUp(27, () => closeOnEscape && onToggle());
+    useClickOutside(closeOnClickOutside, contentRef, onToggle);
 
     return (
         <div className={`${theme.prefixClass}-modal-body`} ref={contentRef}>
