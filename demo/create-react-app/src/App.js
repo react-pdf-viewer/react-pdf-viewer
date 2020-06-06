3d886e7f267a7483ac0f81313a5bbdec32a8901c
Phuoc Nguyen <me@phuoc.ng>
2020-06-06 20:43:38 +0700

    Upgrade pdfjs

--- demo/create-react-app/src/App.js
+++ demo/create-react-app/src/App.js
@@ -5,7 +5,7 @@ import '@phuocng/react-pdf-viewer/cjs/react-pdf-viewer.css';
 
 function App() {
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.3.200/build/pdf.worker.min.js">
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.4.456/build/pdf.worker.min.js">
         <div style={{ height: '750px' }}>
             <Viewer fileUrl={`${process.env.PUBLIC_URL}/pdf-open-parameters.pdf`} />
         </div>
