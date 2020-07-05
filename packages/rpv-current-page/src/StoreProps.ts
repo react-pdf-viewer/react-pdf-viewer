2b3bb613e26a4d87e0ebcec0199047d2d5e85407
Phuoc Nguyen <me@phuoc.ng>
2020-07-05 07:42:58 +0700

    Add props render for current page plugin

--- packages/rpv-current-page/src/StoreProps.ts
+++ packages/rpv-current-page/src/StoreProps.ts
@@ -1,5 +1,14 @@
+/**
+ * A React component to view a PDF document
+ *
+ * @see https://react-pdf-viewer.dev
+ * @license https://react-pdf-viewer.dev/license
+ * @copyright 2019-2020 Nguyen Huu Phuoc <me@phuoc.ng>
+ */
+
 interface StoreProps {
     currentPage?: number;
+    numberOfPages?: number;
 }
 
 export default StoreProps;
