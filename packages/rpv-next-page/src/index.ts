0318b0fea6096e35ccb4e49294bd7dfbb5e2675d
Phuoc Nguyen <me@phuoc.ng>
2020-07-06 23:41:19 +0700

    Properly configure rpv-next-page package

---
+++ packages/rpv-next-page/src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * A React component to view a PDF document
+ *
+ * @see https://react-pdf-viewer.dev
+ * @license https://react-pdf-viewer.dev/license
+ * @copyright 2019-2020 Nguyen Huu Phuoc <me@phuoc.ng>
+ */
+
+import nextPagePlugin from './nextPagePlugin';
+
+export default nextPagePlugin;
