2d892c5921ff3edf49225b31e2dd7a577bc1e555
Phuoc Nguyen <me@phuoc.ng>
2020-07-17 14:41:22 +0700

    Add print plugin

---
+++ packages/rpv-print/src/PrintIcon.tsx
@@ -0,0 +1,30 @@
+/**
+ * A React component to view a PDF document
+ *
+ * @see https://react-pdf-viewer.dev
+ * @license https://react-pdf-viewer.dev/license
+ * @copyright 2019-2020 Nguyen Huu Phuoc <me@phuoc.ng>
+ */
+
+import React from 'react';
+
+import { Icon } from '@phuocng/rpv';
+
+const PrintIcon: React.FC = () => {
+    return (
+        <Icon size={16}>
+            <path
+                d={`M7.5,19.499h9 M7.5,16.499h9 M5.5,16.5h-3c-1.103-0.003-1.997-0.897-2-2v-6c0.003-1.103,0.897-1.997,2-2h19
+                c1.103,0.003,1.997,0.897,2,2v6c-0.003,1.103-0.897,1.997-2,2h-3
+                M5.5,4.5v-4h9.586c0.265,0,0.52,0.105,0.707,0.293l2.414,2.414
+                C18.395,3.394,18.5,3.649,18.5,3.914V4.5
+                M18.5,22.5c0,0.552-0.448,1-1,1h-11c-0.552,0-1-0.448-1-1v-9h13V22.5z
+                M3.5,8.499
+                c0.552,0,1,0.448,1,1s-0.448,1-1,1s-1-0.448-1-1S2.948,8.499,3.5,8.499z
+                M14.5,0.499v4h4`}
+            />
+        </Icon>
+    );
+};
+
+export default PrintIcon;
---
+++ packages/rpv-print/src/PrintProgress.tsx
@@ -0,0 +1,55 @@
+/**
+ * A React component to view a PDF document
+ *
+ * @see https://react-pdf-viewer.dev
+ * @license https://react-pdf-viewer.dev/license
+ * @copyright 2019-2020 Nguyen Huu Phuoc <me@phuoc.ng>
+ */
+
+import React, { useEffect, useContext } from 'react';
+import { LocalizationContext, PrimaryButton, ProgressBar } from '@phuocng/rpv';
+
+import './printProgress.less';
+
+interface PrintProgressProps {
+    numLoadedPages: number;
+    numPages: number;
+    onCancel(): void;
+    onStartPrinting(): void;
+}
+
+const PrintProgress: React.FC<PrintProgressProps> = ({ numLoadedPages, numPages, onCancel, onStartPrinting }) => {
+    const l10nContext = useContext(LocalizationContext);
+    const progress = Math.floor(numLoadedPages * 100 / numPages);
+    useEffect(() => {
+        if (numLoadedPages === numPages) {
+            onStartPrinting();
+        }
+    }, [numLoadedPages]);
+
+    return (
+        <div className='rpv-print-progress'>
+            <div className='rpv-print-progress-inner'>
+                <div className='rpv-print-progress-message'>
+                    {
+                        (l10nContext && l10nContext.plugins && l10nContext.plugins.preparingDocument)
+                            ? l10nContext.plugins.print.preparingDocument
+                            : 'Preparing document ...'
+                    }
+                </div>
+                <div className='rpv-print-progress-bar'>
+                    <ProgressBar progress={progress} />
+                </div>
+                <PrimaryButton onClick={onCancel}>
+                    {
+                        (l10nContext && l10nContext.plugins && l10nContext.plugins.cancel)
+                            ? l10nContext.plugins.print.cancel
+                            : 'Cancel'
+                    }
+                </PrimaryButton>
+            </div>
+        </div>
+    );
+};
+
+export default PrintProgress;
