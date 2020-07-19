7a4a7e6fd8642212510980794c7474782c95e297
Phuoc Nguyen <me@phuoc.ng>
2020-07-19 17:35:16 +0700

    Remove download from render viewer props

--- packages/rpv/src/index.d.ts
+++ packages/rpv/src/index.d.ts
@@ -245,7 +245,6 @@ export interface RenderViewerProps {
     pageWidth: number;
     rotation: number;
     slot: Slot;
-    download(): void;
     changeScrollMode(mode: ScrollMode): void;
     changeSelectionMode(mode: SelectionMode): void;
     openFile(file: File): void;
