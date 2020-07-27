da20a7626dfb86f1295df87f2b7d16aa556c3e11
Phuoc Nguyen <me@phuoc.ng>
2020-07-27 23:25:51 +0700

    Expose API to get metadata

--- packages/rpv/src/index.d.ts
+++ packages/rpv/src/index.d.ts
@@ -17,6 +17,8 @@ export declare namespace PdfJs {
 
     interface PdfDocument {
         numPages: number;
+        getDownloadInfo(): Promise<{ length: number }>;
+        getMetadata(): Promise<MetaData>;
         getPage(pageIndex: number): Promise<Page>;
     }
 
@@ -54,6 +56,23 @@ export declare namespace PdfJs {
     interface Page {
         getViewport(params: ViewPortParams): ViewPort;
         render(params: PageRenderParams): PageRenderTask;
+    }
+
+    // Metadata
+    interface MetaData {
+        contentDispositionFilename?: string;
+        info: MetaDataInfo;
+    }
+    interface MetaDataInfo {
+        Author: string;
+        CreationDate: string;
+        Creator: string;
+        Keywords: string;
+        ModDate: string;
+        PDFFormatVersion: string;
+        Producer: string;
+        Subject: string;
+        Title: string;
     }
 }
 
