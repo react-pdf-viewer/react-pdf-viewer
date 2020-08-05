836a5bbde3e9e8acb9174bebcbf3f8e817e36ccc
Phuoc Nguyen <me@phuoc.ng>
2020-08-05 23:24:24 +0700

    Remove bookmark tab from sidebar

--- packages/rpv/src/layouts/Sidebar.tsx
+++ packages/rpv/src/layouts/Sidebar.tsx
@@ -9,15 +9,12 @@
 import React, { useContext, useState } from 'react';
 
 import AttachmentLoader from '../attachment/AttachmentLoader';
-import BookmarkLoader from '../bookmark/BookmarkLoader';
 import Button from '../components/Button';
-import BookmarkIcon from '../icons/BookmarkIcon';
 import FileIcon from '../icons/FileIcon';
 import LocalizationContext from '../localization/LocalizationContext';
 import LocalizationMap from '../localization/LocalizationMap';
 import Position from '../portal/Position';
 import Tooltip from '../portal/Tooltip';
-import SpecialZoomLevel from '../SpecialZoomLevel';
 import ThemeContext from '../theme/ThemeContext';
 import classNames from '../utils/classNames';
 import PdfJs from '../vendors/PdfJs';
@@ -25,41 +22,25 @@ import './sidebar.less';
 
 interface SidebarProps {
     doc: PdfJs.PdfDocument;
-    onJumpToDest(pageIndex: number, bottomOffset: number, scaleTo: number | SpecialZoomLevel): void;
 }
 
 enum Tab {
     Attachment = 'Attachment',
-    Bookmark = 'Bookmark',
 }
 
 const TOOLTIP_OFFSET = { left: 0, top: 8 };
 
-const Sidebar: React.FC<SidebarProps> = ({ doc, onJumpToDest }) => {
+const Sidebar: React.FC<SidebarProps> = ({ doc }) => {
     const l10n = useContext(LocalizationContext);
     const theme = useContext(ThemeContext);
-    const [tab, setTab] = useState(Tab.Bookmark);
-    const clickBookmarkTab = (): void => setTab(Tab.Bookmark);
+    const [tab, setTab] = useState(Tab.Attachment);
     const clickAttachmentTab = (): void => setTab(Tab.Attachment);
 
     const renderAttachmentTip = (): LocalizationMap => l10n.sidebar.attachment;
-    const renderBookmarkTip = (): LocalizationMap => l10n.sidebar.bookmark;
 
     return (
         <div className={`${theme.prefixClass}-sidebar`}>
             <div className={`${theme.prefixClass}-sidebar-tabs`}>
-                <div className={`${theme.prefixClass}-sidebar-tab`}>
-                    <Tooltip
-                        position={Position.BottomCenter}
-                        target={(
-                            <Button onClick={clickBookmarkTab} isSelected={tab === Tab.Bookmark}>
-                                <BookmarkIcon />
-                            </Button>
-                        )}
-                        content={renderBookmarkTip}
-                        offset={TOOLTIP_OFFSET}
-                    />
-                </div>
                 <div className={`${theme.prefixClass}-sidebar-tab`}>
                     <Tooltip
                         position={Position.BottomCenter}
@@ -80,9 +61,6 @@ const Sidebar: React.FC<SidebarProps> = ({ doc, onJumpToDest }) => {
                     })
                 }
             >
-                {
-                    tab === Tab.Bookmark && <BookmarkLoader doc={doc} onJumpToDest={onJumpToDest} />
-                }
                 {
                     tab === Tab.Attachment && <AttachmentLoader doc={doc} />
                 }
