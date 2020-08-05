e6da3850c7f64479b64d3c39bd7510bdc7206c06
Phuoc Nguyen <me@phuoc.ng>
2020-08-05 21:31:20 +0700

    Remove thumbnail tab from sidebar

--- packages/rpv/src/layouts/Sidebar.tsx
+++ packages/rpv/src/layouts/Sidebar.tsx
@@ -13,41 +13,32 @@ import BookmarkLoader from '../bookmark/BookmarkLoader';
 import Button from '../components/Button';
 import BookmarkIcon from '../icons/BookmarkIcon';
 import FileIcon from '../icons/FileIcon';
-import WrappedScrollingIcon from '../icons/WrappedScrollingIcon';
 import LocalizationContext from '../localization/LocalizationContext';
 import LocalizationMap from '../localization/LocalizationMap';
 import Position from '../portal/Position';
 import Tooltip from '../portal/Tooltip';
 import SpecialZoomLevel from '../SpecialZoomLevel';
 import ThemeContext from '../theme/ThemeContext';
-import ThumbnailList from '../thumbnail/ThumbnailList';
 import classNames from '../utils/classNames';
 import PdfJs from '../vendors/PdfJs';
 import './sidebar.less';
 
 interface SidebarProps {
-    currentPage: number;
     doc: PdfJs.PdfDocument;
-    height: number;
-    rotation: number;
-    width: number;
     onJumpToDest(pageIndex: number, bottomOffset: number, scaleTo: number | SpecialZoomLevel): void;
-    onJumpToPage(pageIndex: number): void;
 }
 
 enum Tab {
     Attachment = 'Attachment',
     Bookmark = 'Bookmark',
-    Thumbnail = 'Thumbnail',
 }
 
 const TOOLTIP_OFFSET = { left: 0, top: 8 };
 
-const Sidebar: React.FC<SidebarProps> = ({ currentPage, doc, height, rotation, width, onJumpToDest, onJumpToPage }) => {
+const Sidebar: React.FC<SidebarProps> = ({ doc, onJumpToDest }) => {
     const l10n = useContext(LocalizationContext);
     const theme = useContext(ThemeContext);
-    const [tab, setTab] = useState(Tab.Thumbnail);
-    const clickThumbnailTab = (): void => setTab(Tab.Thumbnail);
+    const [tab, setTab] = useState(Tab.Bookmark);
     const clickBookmarkTab = (): void => setTab(Tab.Bookmark);
     const clickAttachmentTab = (): void => setTab(Tab.Attachment);
 
@@ -58,18 +49,6 @@ const Sidebar: React.FC<SidebarProps> = ({ currentPage, doc, height, rotation, w
     return (
         <div className={`${theme.prefixClass}-sidebar`}>
             <div className={`${theme.prefixClass}-sidebar-tabs`}>
-                <div className={`${theme.prefixClass}-sidebar-tab`}>
-                    <Tooltip
-                        position={Position.BottomCenter}
-                        target={(
-                            <Button onClick={clickThumbnailTab} isSelected={tab === Tab.Thumbnail}>
-                                <WrappedScrollingIcon />
-                            </Button>
-                        )}
-                        content={renderThumbnailTip}
-                        offset={TOOLTIP_OFFSET}
-                    />
-                </div>
                 <div className={`${theme.prefixClass}-sidebar-tab`}>
                     <Tooltip
                         position={Position.BottomCenter}
@@ -103,18 +82,6 @@ const Sidebar: React.FC<SidebarProps> = ({ currentPage, doc, height, rotation, w
                     })
                 }
             >
-                {
-                    tab === Tab.Thumbnail && (
-                        <ThumbnailList
-                            currentPage={currentPage}
-                            doc={doc}
-                            pageHeight={height}
-                            pageWidth={width}
-                            rotation={rotation}
-                            onJumpToPage={onJumpToPage}
-                        />
-                    )
-                }
                 {
                     tab === Tab.Bookmark && <BookmarkLoader doc={doc} onJumpToDest={onJumpToDest} />
                 }
