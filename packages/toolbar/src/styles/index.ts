ce68e20dab3d9965a249b20ad0df4b032c7c5b9f
Phuoc Nguyen <me@phuoc.ng>
2020-08-17 22:31:19 +0700

    Move styles

---
+++ packages/toolbar/src/styles/index.ts
@@ -0,0 +1,20 @@
+/**
+ * A React component to view a PDF document
+ *
+ * @see https://react-pdf-viewer.dev
+ * @license https://react-pdf-viewer.dev/license
+ * @copyright 2019-2020 Nguyen Huu Phuoc <me@phuoc.ng>
+ */
+
+import '@react-pdf-viewer/drop/styles';
+import '@react-pdf-viewer/full-screen/styles';
+import '@react-pdf-viewer/open/styles';
+import '@react-pdf-viewer/page-navigation/styles';
+import '@react-pdf-viewer/print/styles';
+import '@react-pdf-viewer/properties/styles';
+import '@react-pdf-viewer/scroll-mode/styles';
+import '@react-pdf-viewer/search/styles';
+import '@react-pdf-viewer/selection-mode/styles';
+import '@react-pdf-viewer/zoom/styles';
+
+import './defaultToolbar.less';
