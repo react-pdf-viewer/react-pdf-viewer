6e77c93176511798c7412ee889fa118da8d1f1f3
Phuoc Nguyen <me@phuoc.ng>
2020-08-18 23:23:56 +0700

    Import the main entry point of styles

--- packages/default-layout/src/styles/index.ts
+++ packages/default-layout/src/styles/index.ts
@@ -6,10 +6,10 @@
  * @copyright 2019-2020 Nguyen Huu Phuoc <me@phuoc.ng>
  */
 
-import '@react-pdf-viewer/attachment/styles/index.css';
-import '@react-pdf-viewer/bookmark/styles/index.css';
-import '@react-pdf-viewer/thumbnail/styles/index.css';
-import '@react-pdf-viewer/toolbar/styles/index.css';
+import '@react-pdf-viewer/attachment/styles';
+import '@react-pdf-viewer/bookmark/styles';
+import '@react-pdf-viewer/thumbnail/styles';
+import '@react-pdf-viewer/toolbar/styles';
 
 import './defaultLayout.less';
 import './sidebar.less';
--- packages/toolbar/src/styles/index.ts
+++ packages/toolbar/src/styles/index.ts
@@ -6,15 +6,15 @@
  * @copyright 2019-2020 Nguyen Huu Phuoc <me@phuoc.ng>
  */
 
-import '@react-pdf-viewer/drop/styles/index.css';
-import '@react-pdf-viewer/full-screen/styles/index.css';
-import '@react-pdf-viewer/open/styles/index.css';
-import '@react-pdf-viewer/page-navigation/styles/index.css';
-import '@react-pdf-viewer/print/styles/index.css';
-import '@react-pdf-viewer/properties/styles/index.css';
-import '@react-pdf-viewer/scroll-mode/styles/index.css';
-import '@react-pdf-viewer/search/styles/index.css';
-import '@react-pdf-viewer/selection-mode/styles/index.css';
-import '@react-pdf-viewer/zoom/styles/index.css';
+import '@react-pdf-viewer/drop/styles';
+import '@react-pdf-viewer/full-screen/styles';
+import '@react-pdf-viewer/open/styles';
+import '@react-pdf-viewer/page-navigation/styles';
+import '@react-pdf-viewer/print/styles';
+import '@react-pdf-viewer/properties/styles';
+import '@react-pdf-viewer/scroll-mode/styles';
+import '@react-pdf-viewer/search/styles';
+import '@react-pdf-viewer/selection-mode/styles';
+import '@react-pdf-viewer/zoom/styles';
 
 import './defaultToolbar.less';
