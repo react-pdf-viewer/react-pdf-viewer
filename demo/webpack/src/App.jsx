1b7d63d0db472680e22e06bf70d9cd436fc9c49b
Phuoc Nguyen <me@phuoc.ng>
2020-10-07 19:10:04 +0700

    Update dependency pdfjs-dist

--- demo/webpack/src/App.jsx
+++ demo/webpack/src/App.jsx
@@ -5,7 +5,7 @@ import '@phuocng/react-pdf-viewer/cjs/react-pdf-viewer.css';
 
 const App = () => {
     return (
-        <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.4.456/build/pdf.worker.min.js">
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.5.207/build/pdf.worker.min.js">
             <div
                 style={{
                     height: '750px',
