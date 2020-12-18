95a5876431810d2e1cf5f381ca9562d36d582db5
Phuoc Nguyen <me@phuoc.ng>
2020-12-18 11:57:49 +0700

    Read test document

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
 module.exports = {
+    testEnvironment: 'jsdom',
     moduleNameMapper: {
         // In order to load the PDF document in node, we use the ES5 build 
         'pdfjs-dist': '<rootDir>/node_modules/pdfjs-dist/es5/build/pdf',
-        'file:///assets/(.*)': [
-            'file:///<rootDir>/assets/$1',
-        ],
     },
     setupFilesAfterEnv: [
         '<rootDir>/configs/setupTests.ts',
--- packages/core/__tests__/Viewer.tsx
+++ packages/core/__tests__/Viewer.tsx
@@ -1,7 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
 import * as React from 'react';
 import { render, waitFor } from '@testing-library/react';
 
 import Viewer from '../src/Viewer';
+
+const windowIntersectionObserver = window.IntersectionObserver;
+const intersectionObserverMock = {
+    observe: jest.fn(),
+    disconnect: jest.fn(),
+    unobserve: jest.fn(),
+};
+
+beforeAll(() => {
+    (window as any).IntersectionObserver = jest.fn(() => intersectionObserverMock);
+});
+
+afterAll(() => {
+    window.IntersectionObserver = windowIntersectionObserver;
+});
 
 test('render document that does not exist', async () => {
     const App = () => (
@@ -18,10 +36,11 @@ test('render document that does not exist', async () => {
 });
 
 test('load document successfully', async () => {
+    const rawSamplePdf = fs.readFileSync(path.resolve(__dirname, '../../../assets/sample.pdf'));
     const App = () => (
         <div style={{ height: '100px' }}>
             <Viewer
-                fileUrl={'file:///assets/sample.pdf'}
+                fileUrl={new Uint8Array(rawSamplePdf)}
             />
         </div>
     );
@@ -29,7 +48,4 @@ test('load document successfully', async () => {
     
     const firstText = await findByText('Adobe Acrobat PDF Files');
     expect(firstText).toHaveClass('rpv-core-text');
-
-    // const pages = await waitFor(() => container.querySelectorAll('.rpv-core-inner-page'));
-    // console.log(pages);
 });
