3fdb9dbddd1aaa361025990ee899c75bc59d3b07
Phuoc Nguyen <me@phuoc.ng>
2021-01-27 22:52:37 +0700

    Test lazy load page

--- packages/core/__tests__/Viewer.test.tsx
+++ packages/core/__tests__/Viewer.test.tsx
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { findByTestId, render, screen } from '@testing-library/react';
 
 import { mockAllIsIntersecting, mockIsIntersecting } from '../../../test-utils/mockIntersectionObserver';
 import Viewer from '../src/Viewer';
@@ -53,5 +53,36 @@ describe('Test Viewer', () => {
 
         const firstText = await findByText('Adobe Acrobat PDF Files');
         expect(firstText).toHaveClass('rpv-core-text');
+    });
+
+    test('Lazy load page', async () => {
+        const rawSamplePdf = fs.readFileSync(path.resolve(__dirname, '../../../assets/pdf-open-parameters.pdf'));
+        const App = () => (
+            <div style={{ height: '100px' }}>
+                <Viewer
+                    fileUrl={new Uint8Array(rawSamplePdf)}
+                />
+            </div>
+        );
+        const { findByText, getByTestId } = render(<App />);
+        mockIsIntersecting(getByTestId('viewer'), true);
+
+        let text = await findByText('Parameters for Opening PDF Files');
+        expect(text).toHaveClass('rpv-core-text');
+
+        // Set the second page as visible
+        mockIsIntersecting(getByTestId('viewer-page-layer-1'), true);
+
+        text = await findByText('2007 Adobe Systems', { exact: false });
+        expect(text).toHaveClass('rpv-core-text');
+        expect(text).toHaveTextContent('© 2007 Adobe Systems Incorporated. All rights reserved.');
+
+        const lastPage = getByTestId('viewer-page-layer-7');
+        expect(lastPage).not.toHaveTextContent('URL examples');
+
+        // Set the last page as visible
+        mockIsIntersecting(lastPage, true);
+        text = await findByText('Acrobat SDK', { exact: false });
+        expect(text).toHaveTextContent('Adobe Acrobat SDK');
     });
 });
