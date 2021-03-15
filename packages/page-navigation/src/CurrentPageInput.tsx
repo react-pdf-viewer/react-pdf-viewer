03d04f80cfb83db7d48dee4f080d2b9df4c1b171
handofthecode <deartovi@gmail.com>
2021-03-15 15:42:52 -0700

    Update CurrentPageInput.tsx
    Allow user to freely edit page input box.

--- packages/page-navigation/src/CurrentPageInput.tsx
+++ packages/page-navigation/src/CurrentPageInput.tsx
@@ -6,84 +6,89 @@
  * @copyright 2019-2021 Nguyen Huu Phuoc <me@phuoc.ng>
  */
 
-import * as React from 'react';
-import { Store } from '@react-pdf-viewer/core';
+import * as React from "react";
+import { Store } from "@react-pdf-viewer/core";
 
-import StoreProps from './StoreProps';
-import useCurrentPage from './useCurrentPage';
-import useNumberOfPages from './useNumberOfPages';
+import StoreProps from "./StoreProps";
+import useCurrentPage from "./useCurrentPage";
+import useNumberOfPages from "./useNumberOfPages";
 
 const CurrentPageInput: React.FC<{
-    store: Store<StoreProps>
+  store: Store<StoreProps>;
 }> = ({ store }) => {
-    const [pageTextboxFocused, setPageTextboxFocused] = React.useState(false);
-    const [editingPage, setEditingPage] = React.useState(0);
+  const [pageTextboxFocused, setPageTextboxFocused] = React.useState(false);
+  const [editingPage, setEditingPage] = React.useState(0);
 
-    const { currentPage } = useCurrentPage(store);
-    const { numberOfPages } = useNumberOfPages(store);
+  const { currentPage } = useCurrentPage(store);
+  const { numberOfPages } = useNumberOfPages(store);
 
-    const changePage = (e: React.ChangeEvent<HTMLInputElement>): void => {
-        const newPage = parseInt(e.target.value, 10);
-        if (newPage > 0 && newPage <= numberOfPages) {
-            setEditingPage(newPage - 1);
-        }
-    };
+  const changePage = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const newPage = parseInt(e.target.value, 10);
+    setEditingPage(newPage - 1);
+  };
 
-    const focusPageTextbox = (): void => {
-        setPageTextboxFocused(true);
-        setEditingPage(currentPage);
-    };
+  const focusPageTextbox = (): void => {
+    setPageTextboxFocused(true);
+    setEditingPage(currentPage);
+  };
 
-    const blurPageTextbox = (): void => {
-        setPageTextboxFocused(false);
-    };
+  const blurPageTextbox = (): void => {
+    setPageTextboxFocused(false);
+  };
 
-    const gotoNextPage = (): void => {
-        const nextPage = currentPage + 1;
-        if (nextPage < numberOfPages) {
-            setEditingPage(nextPage);
-            jumpTo(nextPage);
-        }
-    };
+  const gotoNextPage = (): void => {
+    const nextPage = currentPage + 1;
+    if (nextPage < numberOfPages) {
+      setEditingPage(nextPage);
+      jumpTo(nextPage);
+    }
+  };
 
-    const gotoPreviousPage = (): void => {
-        const previousPage = currentPage - 1;
-        if (previousPage >= 0) {
-            setEditingPage(previousPage);
-            jumpTo(previousPage);
-        }
-    };
-    
-    const jumpTo = (page: number): void => {
-        const jumpToPage = store.get('jumpToPage');
-        if (jumpToPage) {
-            jumpToPage(page);
-        }
-    };
+  const gotoPreviousPage = (): void => {
+    const previousPage = currentPage - 1;
+    if (previousPage >= 0) {
+      setEditingPage(previousPage);
+      jumpTo(previousPage);
+    }
+  };
 
-    const keydownPage = (e: React.KeyboardEvent): void => {
-        switch (e.keyCode) {
-            // Up key is pressed
-            case 38: gotoPreviousPage(); break;
-            // Down key
-            case 40: gotoNextPage(); break;
-            // Enter key
-            case 13: jumpTo(editingPage); break;
-            default: break;
-        }
-    };
+  const jumpTo = (page: number): void => {
+    if (page > 0 && page <= numberOfPages) {
+      const jumpToPage = store.get("jumpToPage");
+      jumpToPage?.call(page);
+    }
+  };
 
-    return (
-        <input
-            className='rpv-current-page-input'
-            type='text'
-            value={pageTextboxFocused ? (editingPage + 1) : (currentPage + 1)}
-            onChange={changePage}
-            onFocus={focusPageTextbox}
-            onBlur={blurPageTextbox}
-            onKeyDown={keydownPage}
-        />
-    );
+  const keydownPage = (e: React.KeyboardEvent): void => {
+    switch (e.keyCode) {
+      // Up key is pressed
+      case 38:
+        gotoPreviousPage();
+        break;
+      // Down key
+      case 40:
+        gotoNextPage();
+        break;
+      // Enter key
+      case 13:
+        jumpTo(editingPage);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <input
+      className="rpv-current-page-input"
+      type="text"
+      value={pageTextboxFocused ? editingPage + 1 : currentPage + 1}
+      onChange={changePage}
+      onFocus={focusPageTextbox}
+      onBlur={blurPageTextbox}
+      onKeyDown={keydownPage}
+    />
+  );
 };
 
 export default CurrentPageInput;
