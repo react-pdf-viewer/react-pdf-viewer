3a9535dc4f49246a33ea6606a9ba1c4aba1cf52e
Phuoc Nguyen <me@phuoc.ng>
2021-07-12 21:17:30 +0700

    Don't close Tooltip when using with Popover

--- packages/core/src/portal/Tooltip.tsx
+++ packages/core/src/portal/Tooltip.tsx
@@ -9,9 +9,8 @@
 import * as React from 'react';
 
 import useKeyUp from '../hooks/useKeyUp';
-import { Toggle, ToggleStatus } from '../hooks/useToggle';
+import useToggle, { ToggleStatus } from '../hooks/useToggle';
 import Offset from './Offset';
-import Portal from './Portal';
 import Position from './Position';
 import TooltipBody from './TooltipBody';
 import uniqueId from '../utils/uniqueId';
@@ -27,43 +26,90 @@ interface TooltipProps {
 }
 
 const Tooltip: React.FC<TooltipProps> = ({ ariaControlsSuffix, content, offset, position, target }) => {
+    const { opened, toggle } = useToggle();
     const targetRef = React.useRef<HTMLDivElement>();
+    const contentRef = React.useRef<HTMLDivElement>();
     const controlsSuffix = ariaControlsSuffix || `${uniqueId()}`;
 
-    const renderTarget = (toggle: Toggle): React.ReactElement => {
-        useKeyUp(27, () => {
-            if (targetRef.current && document.activeElement && targetRef.current.contains(document.activeElement)) {
-                hide();
-            }
-        });
+    useKeyUp(27, () => {
+        if (targetRef.current && document.activeElement && targetRef.current.contains(document.activeElement)) {
+            close();
+        }
+    });
 
-        const show = (): void => {
-            toggle(ToggleStatus.Open);
-        };
-        const hide = (): void => {
-            toggle(ToggleStatus.Close);
-        };
-        return (
+    const open = (): void => {
+        toggle(ToggleStatus.Open);
+    };
+    const close = (): void => {
+        toggle(ToggleStatus.Close);
+    };
+
+    const onBlur = (e: React.FocusEvent) => {
+        // Support the case that the parent element of tooltip contains an element
+        // that is focused on automatically
+        // For example
+        //  <Popover
+        //      renderTarget=() => {
+        //          <Tooltip
+        //              target=<MinimalButton />
+        //          />
+        //      }
+        //      renderContent=() => <Menu />
+        //  />
+        // The following events happen when the tooltip gets focused:
+        // - The tooltip is shown
+        // - The button gets focused. Pressing `Enter` will open the popover
+        // - The first menu item is focused on automatically. It triggers the `blur` event of the tooltip
+        // At this moment, we can't destroy the tooltip, because it will destroy the popover too.
+        // Instead, we hide it via CSS, and destroy it later
+
+        // We have to use `parentElement` here due to the markup structure
+        //  Popover
+        //      Tooltip's target
+        //      Tooltip's body
+        //      Popover's overlay
+        //      Popover's body
+        //          Menu
+        const shouldHideTooltip =
+            e.relatedTarget instanceof HTMLElement &&
+            e.currentTarget.parentElement &&
+            e.currentTarget.parentElement.contains(e.relatedTarget);
+
+        if (shouldHideTooltip) {
+            // Hide the tooltip body
+            if (contentRef.current) {
+                contentRef.current.style.display = 'none';
+            }
+        } else {
+            close();
+        }
+    };
+
+    return (
+        <>
             <div
-                aria-describedby={`rpv-core__tooltip-body-${controlsSuffix}`}
                 ref={targetRef}
-                onBlur={hide}
-                onFocus={show}
-                onMouseEnter={show}
-                onMouseLeave={hide}
+                aria-describedby={`rpv-core__tooltip-body-${controlsSuffix}`}
+                onBlur={onBlur}
+                onFocus={open}
+                onMouseEnter={open}
+                onMouseLeave={close}
             >
                 {target}
             </div>
-        );
-    };
-
-    const renderContent = (): React.ReactElement => (
-        <TooltipBody ariaControlsSuffix={controlsSuffix} offset={offset} position={position} targetRef={targetRef}>
-            {content()}
-        </TooltipBody>
+            {opened && (
+                <TooltipBody
+                    ariaControlsSuffix={controlsSuffix}
+                    contentRef={contentRef}
+                    offset={offset}
+                    position={position}
+                    targetRef={targetRef}
+                >
+                    {content()}
+                </TooltipBody>
+            )}
+        </>
     );
-
-    return <Portal target={renderTarget} content={renderContent} />;
 };
 
 export default Tooltip;
