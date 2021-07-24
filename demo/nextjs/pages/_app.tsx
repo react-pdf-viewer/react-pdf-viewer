e8fdba66a60194de6819bcb7c1075d8d11504c6c
Phuoc Nguyen <me@phuoc.ng>
2021-07-24 08:41:44 +0700

    Move global CSS

--- demo/nextjs/pages/_app.tsx
+++ demo/nextjs/pages/_app.tsx
@@ -1,3 +1,6 @@
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
 import '../styles/index.css';
 
 export default function MyApp({ Component, pageProps }) {
--- demo/nextjs/pages/index.tsx
+++ demo/nextjs/pages/index.tsx
@@ -2,9 +2,6 @@ import * as React from 'react';
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-
 const Index = () => {
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
     const handleSwitchTheme = (theme: string) => {
