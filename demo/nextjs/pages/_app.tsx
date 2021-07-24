9dfe3566c039cb133eb392b7fb251d39c6137997
Phuoc Nguyen <me@phuoc.ng>
2021-07-24 08:35:04 +0700

    Use nextjs for demo package

---
+++ demo/nextjs/pages/_app.tsx
@@ -0,0 +1,7 @@
+import '../styles/index.css';
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <Component {...pageProps} />
+    );
+}
--- demo/nextjs/pages/index.tsx
+++ demo/nextjs/pages/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import { Viewer } from '@react-pdf-viewer/core';
+import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
-const App = () => {
+const Index = () => {
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
     const handleSwitchTheme = (theme: string) => {
         localStorage.setItem('theme', theme);
@@ -13,15 +13,17 @@ const App = () => {
     const theme = localStorage.getItem('theme') || '';
 
     return (
-        <Viewer
-            fileUrl='pdf-open-parameters.pdf'               
-            plugins={[
-                defaultLayoutPluginInstance,
-            ]}
-            theme={theme}
-            onSwitchTheme={handleSwitchTheme}
-        />
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.8.335/build/pdf.worker.js">
+            <Viewer
+                fileUrl='pdf-open-parameters.pdf'
+                plugins={[
+                    defaultLayoutPluginInstance,
+                ]}
+                theme={theme}
+                onSwitchTheme={handleSwitchTheme}
+            />
+        </Worker>
     );
 };
 
-export default App;
+export default Index;
