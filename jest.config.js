ac955686f500b0a51aa8e5abf2b01130b1360771
Phuoc Nguyen <me@phuoc.ng>
2021-07-25 14:39:11 +0700

    Add a sample testMatch option

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     testEnvironment: 'jsdom',
+    // Uncomment the `testMatch` option when we want to run a specific test case    
+    // testMatch: [
+    //     '<rootDir>/packages/core/__tests__/changeFileUrl.test.tsx',
+    // ],
     moduleNameMapper: {
         // In order to load the PDF document in node, we use the ES5 build
         // For pdfjs-dist 2.7.570+
