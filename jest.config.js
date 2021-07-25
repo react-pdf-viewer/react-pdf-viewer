8f7aeb130210a14d2385580f2efcd937860fb3b3
Phuoc Nguyen <me@phuoc.ng>
2021-07-25 14:23:28 +0700

    Fix vendor path

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@ module.exports = {
     testEnvironment: 'jsdom',
     moduleNameMapper: {
         // In order to load the PDF document in node, we use the ES5 build
-        'pdfjs-dist': '<rootDir>/node_modules/pdfjs-dist/legacy/build/pdf',
+        // For pdfjs-dist 2.7.570+
+        // 'pdfjs-dist': '<rootDir>/node_modules/pdfjs-dist/legacy/build/pdf',
+        // For pdfjs-dist 2.6.347
+        'pdfjs-dist': '<rootDir>/node_modules/pdfjs-dist/es5/build/pdf',
     },
     setupFilesAfterEnv: ['<rootDir>/configs/setupTests.ts'],
     transform: {
