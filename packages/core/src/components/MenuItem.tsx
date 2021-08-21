1f9d596641f4446de973981eaed5eb0a703bcd69
Phuoc Nguyen <me@phuoc.ng>
2021-08-21 17:53:23 +0700

    feat: Adjust menu item to support RTL

--- packages/core/src/components/MenuItem.tsx
+++ packages/core/src/components/MenuItem.tsx
@@ -9,6 +9,7 @@
 import * as React from 'react';
 
 import { CheckIcon } from '../icons/CheckIcon';
+import { TextDirection, ThemeContext } from '../theme/ThemeContext';
 import { classNames } from '../utils/classNames';
 
 export const MenuItem: React.FC<{
@@ -16,19 +17,50 @@ export const MenuItem: React.FC<{
     icon?: React.ReactElement;
     isDisabled?: boolean;
     onClick(): void;
-}> = ({ checked = false, children, icon = null, isDisabled = false, onClick }) => (
-    <button
-        className={classNames({
-            'rpv-core__menu-item': true,
-            'rpv-core__menu-item--disabled': isDisabled,
-        })}
-        role="menuitem"
-        tabIndex={-1}
-        type="button"
-        onClick={onClick}
-    >
-        <div className="rpv-core__menu-item-icon">{icon}</div>
-        <div className="rpv-core__menu-item-label">{children}</div>
-        <div className="rpv-core__menu-item-check">{checked && <CheckIcon />}</div>
-    </button>
-);
+}> = ({ checked = false, children, icon = null, isDisabled = false, onClick }) => {
+    const { direction } = React.useContext(ThemeContext);
+    const isRtl = direction === TextDirection.RightToLeft;
+
+    return (
+        <button
+            className={classNames({
+                'rpv-core__menu-item': true,
+                'rpv-core__menu-item--disabled': isDisabled,
+                'rpv-core__menu-item--ltr': !isRtl,
+                'rpv-core__menu-item--rtl': isRtl,
+            })}
+            role="menuitem"
+            tabIndex={-1}
+            type="button"
+            onClick={onClick}
+        >
+            <div 
+                className={classNames({
+                    'rpv-core__menu-item-icon': true,
+                    'rpv-core__menu-item-icon--ltr': !isRtl,
+                    'rpv-core__menu-item-icon--rtl': isRtl,
+                })}
+            >
+                {icon}
+            </div>
+            <div 
+                className={classNames({
+                    'rpv-core__menu-item-label': true,
+                    'rpv-core__menu-item-label--ltr': !isRtl,
+                    'rpv-core__menu-item-label--rtl': isRtl,
+                })}
+            >
+                {children}
+            </div>
+            <div
+                className={classNames({
+                    'rpv-core__menu-item-check': true,
+                    'rpv-core__menu-item-check--ltr': !isRtl,
+                    'rpv-core__menu-item-check--rtl': isRtl,
+                })}            
+            >
+                {checked && <CheckIcon />}
+            </div>
+        </button>
+    );
+};
