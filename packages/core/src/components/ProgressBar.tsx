7fbb32bffb31501e6b16aad1ec589145628abc23
Phuoc Nguyen <me@phuoc.ng>
2021-08-26 14:38:54 +0700

    feat: Progress bar supports RTL

--- packages/core/src/components/ProgressBar.tsx
+++ packages/core/src/components/ProgressBar.tsx
@@ -8,12 +8,27 @@
 
 import * as React from 'react';
 
+import { TextDirection, ThemeContext } from '../theme/ThemeContext';
+import { classNames } from '../utils/classNames';
+
 export const ProgressBar: React.FC<{
     progress: number;
-}> = ({ progress }) => (
-    <div className="rpv-core__progress-bar">
-        <div className="rpv-core__progress-bar-progress" style={{ width: `${progress}%` }}>
-            {progress}%
+}> = ({ progress }) => {
+    const { direction } = React.useContext(ThemeContext);
+    const isRtl = direction === TextDirection.RightToLeft;
+
+    return (
+        <div
+            className={
+                classNames({
+                    'rpv-core__progress-bar': true,
+                    'rpv-core__progress-bar--rtl': isRtl,
+                })
+            }
+        >
+            <div className="rpv-core__progress-bar-progress" style={{ width: `${progress}%` }}>
+                {progress}%
+            </div>
         </div>
-    </div>
-);
+    );
+};
