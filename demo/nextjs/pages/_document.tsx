784b6f64b72a2b0edb26c05b89acb9d8b4623063
Phuoc Nguyen <me@phuoc.ng>
2021-08-26 13:17:42 +0700

    feat: Canvas layer always uses the ltr direction

--- demo/nextjs/pages/_document.tsx
+++ demo/nextjs/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html dir="rtl">
                 <Head />
                 <body>
                     <Main />
