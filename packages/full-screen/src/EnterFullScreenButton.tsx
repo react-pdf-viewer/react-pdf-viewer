93a510ed324f51b90cf52377acdcd1b9f7e4d442
Phuoc Nguyen <me@phuoc.ng>
2021-09-04 11:53:18 +0700

    fix: Use Meta key

--- packages/full-screen/src/EnterFullScreenButton.tsx
+++ packages/full-screen/src/EnterFullScreenButton.tsx
@@ -19,7 +19,7 @@ export const EnterFullScreenButton: React.FC<{
 }> = ({ enableShortcuts, onClick }) => {
     const { l10n } = React.useContext(LocalizationContext);
     const label = l10n && l10n.fullScreen ? l10n.fullScreen.enterFullScreen : 'Full screen';
-    const ariaKeyShortcuts = enableShortcuts ? (isMac() ? 'Command+Ctrl+F' : 'F11') : '';
+    const ariaKeyShortcuts = enableShortcuts ? (isMac() ? 'Meta+Ctrl+F' : 'F11') : '';
 
     return (
         <Tooltip
