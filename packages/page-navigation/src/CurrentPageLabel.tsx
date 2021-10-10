f6c6d202fec8584a9dec4ca73399bca5fcc9064d
Phuoc Nguyen <me@phuoc.ng>
2021-10-10 22:16:09 +0700

    feat: Add page label to CurrentPageLabel

--- packages/page-navigation/src/CurrentPageLabel.tsx
+++ packages/page-navigation/src/CurrentPageLabel.tsx
@@ -10,12 +10,14 @@ import * as React from 'react';
 import type { Store } from '@react-pdf-viewer/core';
 
 import { useCurrentPage } from './useCurrentPage';
+import { useDocument } from './useDocument';
 import { useNumberOfPages } from './useNumberOfPages';
 import type { StoreProps } from './types/StoreProps';
 
 export interface RenderCurrentPageLabelProps {
     currentPage: number;
     numberOfPages: number;
+    pageLabel: string;
 }
 
 type RenderCurrentPageLabel = (props: RenderCurrentPageLabelProps) => React.ReactElement;
@@ -28,14 +30,30 @@ export const CurrentPageLabel: React.FC<{
     children?: RenderCurrentPageLabel;
     store: Store<StoreProps>;
 }> = ({ children, store }) => {
+    const docRef = useDocument(store);
+    const [labels, setLabels] = React.useState(null);
     const { currentPage } = useCurrentPage(store);
     const { numberOfPages } = useNumberOfPages(store);
 
     const defaultChildren = (props: RenderCurrentPageLabelProps) => <>{props.currentPage + 1}</>;
-
     const render = children || defaultChildren;
-    return render({
-        currentPage,
-        numberOfPages,
-    });
+
+    React.useEffect(() => {
+        const doc = docRef.current;
+        if (doc) {
+            doc.getPageLabels().then((labels) => {
+                setLabels(labels || []);
+            });
+        }
+    }, [docRef.current]);
+
+    return labels ? (
+        render({
+            currentPage,
+            numberOfPages,
+            pageLabel: labels.length === numberOfPages ? labels[currentPage] : '',
+        })
+    ) : (
+        <></>
+    );
 };
