98e15a4b310655377df46ea1531172401e831ba8
Phuoc Nguyen <me@phuoc.ng>
2021-10-17 18:13:01 +0700

    feat: Add thumbnail plugin options

--- demo/nextjs/pages/index.tsx
+++ demo/nextjs/pages/index.tsx
@@ -1,40 +1,19 @@
 import * as React from 'react';
-import { SpecialZoomLevel, TextDirection, Viewer, Worker } from '@react-pdf-viewer/core';
+import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-import { thumbnailPlugin } from '@react-pdf-viewer/thumbnail';
 
 const Index = () => {
-    const thumbnailPluginInstance = thumbnailPlugin();
-    const { Thumbnails } = thumbnailPluginInstance;
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
     return (
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.9.359/build/pdf.worker.js">
             <div
                 style={{
-                    border: '1px solid rgba(0, 0, 0, 0.3)',
                     display: 'flex',
-                    height: '100%',
+                    height: '100vh',
                 }}
             >
-                <div
-                    style={{
-                        borderRight: '1px solid rgba(0, 0, 0, 0.3)',
-                        overflow: 'auto',
-                        width: '30%',
-                    }}
-                >
-                    <Thumbnails
-                        renderCurrentPageLabel={(props) => (
-                            <>
-                                {props.pageIndex + 1}
-                                {props.pageLabel !== `${props.pageIndex + 1}` && `(${props.pageLabel})`}
-                            </>
-                        )}
-                    />
-                </div>
-                <div style={{ flex: 1 }}>
-                    <Viewer fileUrl="/ignore/page-labels-2.pdf" plugins={[thumbnailPluginInstance]} />
-                </div>
+                <Viewer fileUrl="/pdf-open-parameters.pdf" plugins={[defaultLayoutPluginInstance]} />
             </div>
         </Worker>
     );
--- jest-e2e.config.js
+++ jest-e2e.config.js
@@ -2,9 +2,7 @@ module.exports = {
     preset: 'jest-puppeteer',
     // Uncomment the `testMatch` option when we want to run a specific test case
     //testMatch: ['<rootDir>/packages/highlight/__tests__/triggerNone.test.ts'],
-    testRegex: [
-        '(/__tests__/.*|(\\.|/)(e2e))\\.ts$'
-    ],
+    testRegex: ['(/__tests__/.*|(\\.|/)(e2e))\\.ts$'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
