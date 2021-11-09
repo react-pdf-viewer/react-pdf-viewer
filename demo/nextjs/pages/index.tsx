37cafe0de6a8e2cc596ffe8828fe80812890bd5b
Phuoc Nguyen <me@phuoc.ng>
2021-11-09 16:56:50 +0700

    feat: Add drop plugin

--- demo/nextjs/pages/index.tsx
+++ demo/nextjs/pages/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { dropPlugin } from '@react-pdf-viewer/drop';
 
 const Index = () => {
-    const defaultLayoutPluginInstance = defaultLayoutPlugin({
-        sidebarTabs: () => [],
-    });
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    const dropPluginInstance = dropPlugin();
 
     return (
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.10.377/build/pdf.worker.js">
@@ -17,7 +17,10 @@ const Index = () => {
                     width: '64rem',
                 }}
             >
-                <Viewer fileUrl="/pdf-open-parameters.pdf" plugins={[defaultLayoutPluginInstance]} />
+                <Viewer
+                    fileUrl="/pdf-open-parameters.pdf"
+                    plugins={[defaultLayoutPluginInstance, dropPluginInstance]}
+                />
             </div>
         </Worker>
     );
