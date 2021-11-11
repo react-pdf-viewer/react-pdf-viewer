14643b42cd53166600b3ab117bdc1130b648a441
Phuoc Nguyen <me@phuoc.ng>
2021-11-11 22:28:29 +0700

    fix: Downloading document doesn't work on iOS Chrome

--- packages/get-file/src/downloadFile.ts
+++ packages/get-file/src/downloadFile.ts
@@ -6,25 +6,50 @@
  * @copyright 2019-2021 Nguyen Huu Phuoc <me@phuoc.ng>
  */
 
-import type { OpenFile, PdfJs } from '@react-pdf-viewer/core';
+import type { PdfJs } from '@react-pdf-viewer/core';
+
+const isChromeIOS = () => /iphone|ipod|ipad/i.test(navigator.userAgent) && /CriOS/i.test(navigator.userAgent);
+
+const isSafariIOS = () => /iphone|ipod|ipad/i.test(navigator.userAgent) && !/CriOS/i.test(navigator.userAgent);
+
+// Encode Uint8Array to base64
+const encodeUint8Array = (data: Uint8Array): string =>
+    btoa(
+        Array(data.length)
+            .fill('')
+            .map((_, i) => String.fromCharCode(data[i]))
+            .join('')
+    );
+
+const download = (url: string, saveAs: string): void => {
+    const link = document.createElement('a');
+    link.style.display = 'none';
+    link.href = url;
+    link.setAttribute('download', saveAs);
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
+
+const downloadBlob = (data: Uint8Array, saveAs: string, mimeType: string): void => {
+    const blobUrl = URL.createObjectURL(new Blob([data], { type: mimeType }));
+    download(blobUrl, saveAs);
+    if (blobUrl) {
+        URL.revokeObjectURL(blobUrl);
+    }
+    return;
+};
 
 export const downloadFile = (doc: PdfJs.PdfDocument, saveAs: string): void => {
     doc.getData().then((data) => {
-        // `application/pdf` is the correct MIME type for a PDF file. However, it's a known supported file type on iOS
-        // and iOS will try to open the file instead of downloading it.
-        // Using `application/octet-stream` forces iOS Safari to download the file
-        const blobUrl = URL.createObjectURL(new Blob([data], { type: 'application/octet-stream' }));
-        const link = document.createElement('a');
-        link.style.display = 'none';
-        link.href = blobUrl;
-        link.setAttribute('download', saveAs);
-
-        document.body.appendChild(link);
-        link.click();
-
-        document.body.removeChild(link);
-        if (blobUrl) {
-            URL.revokeObjectURL(blobUrl);
-        }
+        isSafariIOS()
+            ? // `application/pdf` is the correct MIME type for a PDF file. However, it's a known supported file type on iOS
+              // and iOS will try to open the file instead of downloading it.
+              // Using `application/octet-stream` forces iOS Safari to download the file
+              downloadBlob(data, saveAs, 'application/octet-stream')
+            : isChromeIOS()
+            ? download(`data:application/pdf;base64,${encodeUint8Array(data)}`, saveAs)
+            : downloadBlob(data, saveAs, 'application/pdf');
     });
 };
