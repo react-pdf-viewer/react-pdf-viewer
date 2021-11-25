54de5ab4c6cab04371e519fbbf51c00595cf1976
Phuoc Nguyen <me@phuoc.ng>
2021-11-25 21:24:31 +0700

    feat: Distinguish testId for selection mode buttons/menus

--- packages/selection-mode/src/SwitchSelectionModeMenuItem.tsx
+++ packages/selection-mode/src/SwitchSelectionModeMenuItem.tsx
@@ -10,23 +10,31 @@ import * as React from 'react';
 import { MenuItem } from '@react-pdf-viewer/core';
 
 import { SwitchSelectionModeDecorator } from './SwitchSelectionModeDecorator';
+import { SelectionMode } from './structs/SelectionMode';
 import { RenderSwitchSelectionModeProps } from './types/RenderSwitchSelectionModeProps';
 
 export const SwitchSelectionModeMenuItem: React.FC<RenderSwitchSelectionModeProps> = ({
     isSelected,
     mode,
     onClick,
-}) => (
-    <SwitchSelectionModeDecorator mode={mode} onClick={onClick}>
-        {(props) => (
-            <MenuItem
-                checked={isSelected}
-                icon={props.icon}
-                testId="selection-mode__switch-menu"
-                onClick={props.onClick}
-            >
-                {props.label}
-            </MenuItem>
-        )}
-    </SwitchSelectionModeDecorator>
-);
+}) => {
+    let testId = '';
+    switch (mode) {
+        case SelectionMode.Hand:
+            testId = 'selection-mode__hand-menu';
+            break;
+        case SelectionMode.Text:
+        default:
+            testId = 'selection-mode__text-menu';
+    }
+
+    return (
+        <SwitchSelectionModeDecorator mode={mode} onClick={onClick}>
+            {(props) => (
+                <MenuItem checked={isSelected} icon={props.icon} testId={testId} onClick={props.onClick}>
+                    {props.label}
+                </MenuItem>
+            )}
+        </SwitchSelectionModeDecorator>
+    );
+};
