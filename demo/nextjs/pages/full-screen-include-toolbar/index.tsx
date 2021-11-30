2a0b0f6702de7b2042a86db5357c6c563641ed8f
Phuoc Nguyen <me@phuoc.ng>
2021-11-30 19:03:02 +0700

    feat: Include toolbar when enter the full screen mode

---
+++ demo/nextjs/pages/full-screen-include-toolbar/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Button, Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+const IndexPage = () => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin({
+        toolbarPlugin: {
+            fullScreenPlugin: {
+                getFullScreenTarget: (pagesContainer) => pagesContainer.closest('[data-testid="default-layout__body"]'),
+                renderExitFullScreenButton: (_) => <></>,
+            },
+        },
+    });
+
+    return (
+        <div
+            style={{
+                height: '50rem',
+                margin: '5rem auto',
+                width: '64rem',
+            }}
+        >
+            <Viewer fileUrl="/pdf-open-parameters.pdf" plugins={[defaultLayoutPluginInstance]} />
+        </div>
+    );
+};
+
+export default IndexPage;
