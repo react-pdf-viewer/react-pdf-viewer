1876d32adf1ee7c52460f592c72ef6998d5a559c
Phuoc Nguyen <me@phuoc.ng>
2021-12-05 19:08:44 +0700

    feat: Move renderThumbnailItem to Thumbnails instead of plugin level

--- packages/thumbnail/src/index.d.ts
+++ packages/thumbnail/src/index.d.ts
@@ -10,6 +10,10 @@ import * as React from 'react';
 import type { Plugin } from '@react-pdf-viewer/core';
 
 // Plugin
+export interface ThumbnailsProps {
+    renderThumbnailItem?: RenderThumbnailItem;
+}
+
 export interface CoverProps {
     getPageIndex?({ numPages }: { numPages: number }): number;
 }
@@ -39,12 +43,11 @@ export interface ThumbnailPluginProps {
     // The spinner that replaces the default `Spinner` component
     // For example, it is displayed when loading the cover or thumbnail of a page
     renderSpinner?: () => React.ReactElement;
-    renderThumbnailItem?: RenderThumbnailItem;
 }
 
 export interface ThumbnailPlugin extends Plugin {
     Cover: (props: CoverProps) => React.ReactElement;
-    Thumbnails(): React.ReactElement;
+    Thumbnails(props?: ThumbnailsProps): React.ReactElement;
 }
 
 export function thumbnailPlugin(props?: ThumbnailPluginProps): ThumbnailPlugin;
