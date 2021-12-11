70f181ba0774177962ec5900a40023ba8d140dfb
Phuoc Nguyen <me@phuoc.ng>
2021-12-11 18:56:41 +0700

    feat: Add e2e test for clicking internal links

---
+++ packages/core/__tests__/internalLinks.e2e.ts
@@ -0,0 +1,91 @@
+import 'expect-puppeteer';
+
+test('Jump between internal links', async () => {
+    await page.goto('http://localhost:3000/core');
+    await page.evaluate(() => document.querySelector('[data-testid="core__viewer"]').scrollIntoView());
+
+    // Wait until the first page is rendered
+    await page.waitForSelector('[data-testid="core__page-layer-0"]', { visible: true });
+
+    const pagesContainer = await page.waitForSelector('[data-testid="core__inner-pages"]');
+
+    const jumpToTableOfContents = async () => {
+        await page.evaluate(() => document.querySelector('[data-testid="core__page-layer-2"]').scrollIntoView());
+        // Wait until the page is rendered
+        return await page.waitForSelector('[data-testid="core__text-layer-2"]', { visible: true });
+    };
+
+    // Jump to the 3rd page
+    await jumpToTableOfContents();
+
+    // Click the `Preface` link
+    let link = await page.waitForSelector('[data-annotation-id="31R"] a');
+    await link.click();
+
+    await page.waitForSelector('[data-testid="core__text-layer-3"]', { visible: true });
+    let scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(4197);
+
+    // Click the `Who should read this guide` link
+    await jumpToTableOfContents();
+    link = await page.waitForSelector('[data-annotation-id="37R"] a');
+    await link.click();
+
+    await page.waitForSelector('[data-testid="core__text-layer-3"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(4386);
+
+    // Click the `Related documentation` link
+    await jumpToTableOfContents();
+    link = await page.waitForSelector('[data-annotation-id="38R"] a');
+    await link.click();
+
+    await page.waitForSelector('[data-testid="core__text-layer-3"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(4515);
+
+    // Click the `Parameters for Opening PDF Files` link
+    await jumpToTableOfContents();
+    link = await page.waitForSelector('[data-annotation-id="39R"] a');
+    await link.click();
+
+    await page.waitForSelector('[data-testid="core__text-layer-4"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(5560);
+
+    // Click the `Parameters` link
+    await jumpToTableOfContents();
+    link = await page.waitForSelector('[data-annotation-id="34R"] a');
+    await link.click();
+
+    await page.waitForSelector('[data-testid="core__text-layer-4"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(6131);
+
+    // Click the `Specifying parameters in a URL` link
+    await jumpToTableOfContents();
+    link = await page.waitForSelector('[data-annotation-id="35R"] a');
+    await link.click();
+
+    await page.waitForSelector('[data-testid="core__text-layer-6"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(8990);
+
+    // Click the `URL examples` link
+    await jumpToTableOfContents();
+    link = await page.waitForSelector('[data-annotation-id="36R"] a');
+    await link.click();
+
+    await page.waitForSelector('[data-testid="core__text-layer-7"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(9662);
+
+    // Click the `URL limitations` link
+    await jumpToTableOfContents();
+    link = await page.waitForSelector('[data-annotation-id="33R"] a');
+    await link.click();
+
+    await page.waitForSelector('[data-testid="core__text-layer-7"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(9953);
+});
