ebbedb3afab1790020c912a8f47beed6563261cb
Phuoc Nguyen <me@phuoc.ng>
2021-12-23 15:11:47 +0700

    fix: Exceed the max canvas size

--- packages/core/src/utils/roundToDivide.ts
+++ packages/core/src/utils/roundToDivide.ts
@@ -8,5 +8,5 @@
 
 export const roundToDivide = (a: number, b: number): number => {
     const remainder = a % b;
-    return remainder === 0 ? a : Math.round(a - remainder + b);
+    return remainder === 0 ? a : Math.floor(a - remainder);
 };
