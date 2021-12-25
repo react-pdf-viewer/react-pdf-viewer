f9aa44552c27a8e4ef507d9a3d44ebfacc2915df
Phuoc Nguyen <me@phuoc.ng>
2021-12-25 18:30:33 +0700

    feat: Find max item in array by given key

---
+++ packages/core/src/utils/maxByKey.ts
@@ -0,0 +1,10 @@
+/**
+ * A React component to view a PDF document
+ *
+ * @see https://react-pdf-viewer.dev
+ * @license https://react-pdf-viewer.dev/license
+ * @copyright 2019-2021 Nguyen Huu Phuoc <me@phuoc.ng>
+ */
+
+export const maxByKey = <T extends Record<string, any>, K extends keyof T>(arr: T[], key: K): T =>
+    arr.reduce((a, b) => (a[key] >= b[key] ? a : b), {} as T);
