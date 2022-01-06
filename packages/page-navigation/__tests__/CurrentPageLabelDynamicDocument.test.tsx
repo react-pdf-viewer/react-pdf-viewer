c7ef9142f9a1f5f2c9f720ffda54b572d4abc301
Phuoc Nguyen <me@phuoc.ng>
2022-01-06 21:26:21 +0700

    fix: Test <CurrentPageLabel> with dynamic document

--- packages/page-navigation/__tests__/CurrentPageLabelDynamicDocument.test.tsx
+++ packages/page-navigation/__tests__/CurrentPageLabelDynamicDocument.test.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { fireEvent, render } from '@testing-library/react';
-import { Viewer } from '@react-pdf-viewer/core';
+import { Button, Viewer } from '@react-pdf-viewer/core';
 
 import { mockIsIntersecting } from '../../../test-utils/mockIntersectionObserver';
+import { mockResize } from '../../../test-utils/mockResizeObserver';
 import { pageNavigationPlugin } from '../src';
 
 const TestCurrentPageLabelDynamicDocument = () => {
@@ -11,46 +12,87 @@ const TestCurrentPageLabelDynamicDocument = () => {
     const { CurrentPageLabel } = pageNavigationPluginInstance;
 
     return (
-        <>
-            <div style={{ marginRight: '8px' }}>
-                <button onClick={() => setFileUrl(global['__OPEN_PARAMS_PDF__'])}>Load document 1</button>
-                <button onClick={() => setFileUrl(global['__MULTIPLE_PAGES_PDF__'])}>Load document 2</button>
+        <div>
+            <div style={{ display: 'flex', marginBottom: '0.5rem' }}>
+                <div style={{ marginRight: '0.5rem' }}>
+                    <Button onClick={() => setFileUrl(global['__OPEN_PARAMS_PDF__'])}>Load document 1</Button>
+                </div>
+                <Button onClick={() => setFileUrl(global['__MULTIPLE_PAGES_PDF__'])}>Load document 2</Button>
             </div>
-            <div style={{ height: '720px' }}>
-                {fileUrl && (
-                    <div>
-                        <div data-testid="current-page-label">
-                            <CurrentPageLabel>
-                                {(props) => (
-                                    <>
-                                        {props.currentPage + 1} of {props.numberOfPages}
-                                    </>
-                                )}
-                            </CurrentPageLabel>
-                        </div>
+
+            {fileUrl && (
+                <div
+                    style={{
+                        border: '1px solid rgba(0, 0, 0, 0.3)',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: '50rem',
+                        width: '50rem',
+                    }}
+                >
+                    <div
+                        style={{
+                            alignItems: 'center',
+                            borderBottom: '1px solid rgba(0, 0, 0, 0.3)',
+                            display: 'flex',
+                            height: '2rem',
+                            justifyContent: 'center',
+                        }}
+                        data-testid="current-page-label"
+                    >
+                        <CurrentPageLabel>
+                            {(props) => (
+                                <>
+                                    {props.currentPage + 1} of {props.numberOfPages}
+                                </>
+                            )}
+                        </CurrentPageLabel>
+                    </div>
+                    <div style={{ flex: 1, overflow: 'hidden' }}>
                         <Viewer fileUrl={fileUrl} plugins={[pageNavigationPluginInstance]} />
                     </div>
-                )}
-            </div>
-        </>
+                </div>
+            )}
+        </div>
     );
 };
 
 test('Test <CurrentPageLabel> with dynamic document', async () => {
-    const { findByTestId, getByText } = render(<TestCurrentPageLabelDynamicDocument />);
+    const { findByTestId, getByTestId, getByText } = render(<TestCurrentPageLabelDynamicDocument />);
 
     // Click the `Load document 1` button
     fireEvent.click(getByText('Load document 1'));
 
     let viewerEle = await findByTestId('core__viewer');
     mockIsIntersecting(viewerEle, true);
+    viewerEle['__jsdomMockClientHeight'] = 767;
+    viewerEle['__jsdomMockClientWidth'] = 800;
 
     let pageLabel = await findByTestId('current-page-label');
     expect(pageLabel.textContent).toEqual('1 of 8');
 
     // Jump to the fourth page
-    let page = await findByTestId('core__page-layer-3');
-    mockIsIntersecting(page, true);
+    let pagesContainer = getByTestId('core__inner-pages');
+    pagesContainer.getBoundingClientRect = jest.fn(() => ({
+        x: 0,
+        y: 0,
+        height: 767,
+        width: 800,
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        toJSON: () => {},
+    }));
+    mockResize(pagesContainer);
+
+    fireEvent.scroll(pagesContainer, {
+        target: {
+            scrollTop: 2662,
+        },
+    });
+
+    await findByTestId('core__page-layer-3');
     pageLabel = await findByTestId('current-page-label');
     expect(pageLabel.textContent).toEqual('4 of 8');
 
@@ -59,13 +101,34 @@ test('Test <CurrentPageLabel> with dynamic document', async () => {
 
     viewerEle = await findByTestId('core__viewer');
     mockIsIntersecting(viewerEle, true);
+    viewerEle['__jsdomMockClientHeight'] = 767;
+    viewerEle['__jsdomMockClientWidth'] = 800;
 
     pageLabel = await findByTestId('current-page-label');
     expect(pageLabel.textContent).toEqual('1 of 2');
 
     // Jump to the second page
-    page = await findByTestId('core__page-layer-1');
-    mockIsIntersecting(page, true);
+    pagesContainer = getByTestId('core__inner-pages');
+    pagesContainer.getBoundingClientRect = jest.fn(() => ({
+        x: 0,
+        y: 0,
+        height: 767,
+        width: 800,
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        toJSON: () => {},
+    }));
+    mockResize(pagesContainer);
+
+    fireEvent.scroll(pagesContainer, {
+        target: {
+            scrollTop: 887,
+        },
+    });
+
+    await findByTestId('core__page-layer-1');
     pageLabel = await findByTestId('current-page-label');
     expect(pageLabel.textContent).toEqual('2 of 2');
 });
