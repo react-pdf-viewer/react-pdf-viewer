0f546cb5ba57d74d5f9fc3bb1ef28bc15fa36770
Phuoc Nguyen <me@phuoc.ng>
2022-01-07 16:57:03 +0700

    fix: Keep highlighting after zooming

--- packages/search/__tests__/keepHighlightAfterZoomDefaultLayout.test.tsx
+++ packages/search/__tests__/keepHighlightAfterZoomDefaultLayout.test.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { findAllByTitle } from '@testing-library/dom';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, waitForElementToBeRemoved } from '@testing-library/react';
 
 import { Viewer } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import { mockIsIntersecting } from '../../../test-utils/mockIntersectionObserver';
+import { mockResize } from '../../../test-utils/mockResizeObserver';
 import type { FlagKeyword } from '../src/types/FlagKeyword';
 
 const TestKeepHighlight: React.FC<{
@@ -39,12 +40,36 @@ test('Keep highlighting after zooming', async () => {
         <TestKeepHighlight fileUrl={global['__OPEN_PARAMS_PDF__']} keyword={keyword} />
     );
 
-    mockIsIntersecting(getByTestId('core__viewer'), true);
+    const viewerEle = getByTestId('core__viewer');
+    mockIsIntersecting(viewerEle, true);
+    viewerEle['__jsdomMockClientHeight'] = 800;
+    viewerEle['__jsdomMockClientWidth'] = 800;
 
-    let page = await findByTestId('core__page-layer-4');
-    mockIsIntersecting(page, true);
+    // Wait until the document is loaded completely
+    await waitForElementToBeRemoved(() => getByTestId('core__doc-loading'));
+
+    const pagesContainer = getByTestId('core__inner-pages');
+    pagesContainer.getBoundingClientRect = jest.fn(() => ({
+        x: 0,
+        y: 0,
+        height: 800,
+        width: 800,
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        toJSON: () => {},
+    }));
+    mockResize(pagesContainer);
+
+    fireEvent.scroll(pagesContainer, {
+        target: {
+            scrollTop: 3549,
+        },
+    });
 
     // Wait for the text layer to be rendered completely
+    let page = await findByTestId('core__page-layer-4');
     await findByTestId('core__text-layer-4');
 
     let highlights = await findAllByTitle(page, keyword);
@@ -56,8 +81,13 @@ test('Keep highlighting after zooming', async () => {
     const zoomOutButton = await findByTestId('zoom__out-button');
     fireEvent.click(zoomOutButton);
 
+    fireEvent.scroll(pagesContainer, {
+        target: {
+            scrollTop: 4440,
+        },
+    });
+
     page = await findByTestId('core__page-layer-5');
-    mockIsIntersecting(page, true);
     await findByTestId('core__text-layer-5');
 
     highlights = await findAllByTitle(page, keyword);
