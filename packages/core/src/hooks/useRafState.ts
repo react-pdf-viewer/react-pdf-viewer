8f58abba901d6a59aab3c6cf3043ccd759f2354a
Phuoc Nguyen <me@phuoc.ng>
2022-01-19 22:14:47 +0700

    fix: Check if the component is unmounted

--- packages/core/src/hooks/useRafState.ts
+++ packages/core/src/hooks/useRafState.ts
@@ -8,14 +8,17 @@
 
 import * as React from 'react';
 
+import { useIsMounted } from './useIsMounted';
+
 export const useRafState = <T>(initialState: T | (() => T)): [T, React.Dispatch<React.SetStateAction<T>>] => {
+    const isMounted = useIsMounted();
     const rafRef = React.useRef(0);
     const [state, setState] = React.useState(initialState);
 
     const setRafState = React.useCallback((value: T | ((prevState: T) => T)) => {
         cancelAnimationFrame(rafRef.current);
         rafRef.current = requestAnimationFrame(() => {
-            setState(value);
+            isMounted.current && setState(value);
         });
     }, []);
 
