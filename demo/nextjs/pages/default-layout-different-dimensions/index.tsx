3d831da619b43c0cde1b3b0ca4736cacc1e0d774
Phuoc Nguyen <me@phuoc.ng>
2022-02-16 16:41:55 +0700

    feat: Add e2e test for document with different page dimensions

---
+++ demo/nextjs/pages/default-layout-different-dimensions/index.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+const IndexPage = () => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                height: '50rem',
+                margin: '5rem auto',
+                width: '64rem',
+            }}
+        >
+            <Viewer fileUrl="/pdf-open-parameters-rotated.pdf" plugins={[defaultLayoutPluginInstance]} />
+        </div>
+    );
+};
+
+export default IndexPage;
---
+++ packages/default-layout/__tests__/differentDimensions.e2e.ts
@@ -0,0 +1,188 @@
+import 'expect-puppeteer';
+
+test('Document with different page dimensions', async () => {
+    await page.goto('http://localhost:3000/default-layout-different-dimensions');
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+    });
+    await page.evaluate(() => document.querySelector('[data-testid="core__viewer"]').scrollIntoView());
+
+    // Wait until the first page is rendered
+    await page.waitForSelector('[data-testid="core__page-layer-0"]', { visible: true });
+
+    // Zoom to 75%
+    const zoomButton = await page.waitForSelector('[data-testid="zoom__popover-target"]');
+    await zoomButton.click();
+
+    const zoomPopover = await page.waitForSelector('[id="rpv-core__popover-body-inner-zoom"]');
+    const zoomMenuItem = await zoomPopover.waitForSelector('button:nth-of-type(5)');
+    const zoomLevel = await zoomMenuItem.evaluate((ele) => ele.textContent);
+    expect(zoomLevel).toEqual('75%');
+    await zoomMenuItem.click();
+
+    // Jump to the 2nd page
+    let nextPageButton = await page.waitForSelector('[data-testid="page-navigation__next-button"]');
+    await nextPageButton.click();
+
+    const pagesContainer = await page.waitForSelector('[data-testid="core__inner-pages"]');
+    await page.waitForSelector('[data-testid="core__text-layer-1"]', { visible: true });
+    let scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(610);
+
+    // Jump to the 3rd page
+    await nextPageButton.click();
+    await page.waitForSelector('[data-testid="core__text-layer-2"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(1071);
+
+    // Jump to the 4th page
+    await nextPageButton.click();
+    await page.waitForSelector('[data-testid="core__text-layer-3"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(1681);
+
+    // Jump to the 5th page
+    await nextPageButton.click();
+    await page.waitForSelector('[data-testid="core__text-layer-4"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(2143);
+
+    // Jump to the 6th page
+    await nextPageButton.click();
+    await page.waitForSelector('[data-testid="core__text-layer-5"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(2604);
+
+    // Jump to the 7th page
+    await nextPageButton.click();
+    await page.waitForSelector('[data-testid="core__text-layer-6"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(3214);
+
+    // Jump to the 8th page
+    await nextPageButton.click();
+    await page.waitForSelector('[data-testid="core__text-layer-7"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(3677);
+});
+
+test('Document with different page dimensions (horizontal scroll mode)', async () => {
+    await page.goto('http://localhost:3000/default-layout-different-dimensions');
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+    });
+    await page.evaluate(() => document.querySelector('[data-testid="core__viewer"]').scrollIntoView());
+
+    // Wait until the first page is rendered
+    await page.waitForSelector('[data-testid="core__page-layer-0"]', { visible: true });
+
+    // Switch to the horizontal scroll mode
+    const moreActionsButton = await page.waitForSelector('[data-testid="toolbar__more-actions-popover-target"]');
+    await moreActionsButton.click();
+
+    const switchToHorizontalMenu = await page.waitForSelector('[data-testid="scroll-mode__horizontal-menu"]');
+    await switchToHorizontalMenu.click();
+
+    // Zoom to 75%
+    const zoomButton = await page.waitForSelector('[data-testid="zoom__popover-target"]');
+    await zoomButton.click();
+
+    const zoomPopover = await page.waitForSelector('[id="rpv-core__popover-body-inner-zoom"]');
+    const zoomMenuItem = await zoomPopover.waitForSelector('button:nth-of-type(5)');
+    const zoomLevel = await zoomMenuItem.evaluate((ele) => ele.textContent);
+    expect(zoomLevel).toEqual('75%');
+    await zoomMenuItem.click();
+
+    // Jump to the 2nd page
+    let nextPageButton = await page.waitForSelector('[data-testid="page-navigation__next-button"]');
+    await nextPageButton.click();
+
+    const pagesContainer = await page.waitForSelector('[data-testid="core__inner-pages"]');
+    await page.waitForSelector('[data-testid="core__text-layer-1"]', { visible: true });
+    let scrollLeft = await pagesContainer.evaluate((ele) => ele.scrollLeft);
+    expect(scrollLeft).toEqual(461);
+
+    // Jump to the 3rd page
+    await nextPageButton.click();
+    await page.waitForSelector('[data-testid="core__text-layer-2"]', { visible: true });
+    scrollLeft = await pagesContainer.evaluate((ele) => ele.scrollLeft);
+    expect(scrollLeft).toEqual(1071);
+
+    // Jump to the 4th page
+    await nextPageButton.click();
+    await page.waitForSelector('[data-testid="core__text-layer-3"]', { visible: true });
+    scrollLeft = await pagesContainer.evaluate((ele) => ele.scrollLeft);
+    expect(scrollLeft).toEqual(1533);
+
+    // Jump to the 5th page
+    await nextPageButton.click();
+    await page.waitForSelector('[data-testid="core__text-layer-4"]', { visible: true });
+    scrollLeft = await pagesContainer.evaluate((ele) => ele.scrollLeft);
+    expect(scrollLeft).toEqual(2143);
+
+    // Jump to the 6th page
+    await nextPageButton.click();
+    await page.waitForSelector('[data-testid="core__text-layer-5"]', { visible: true });
+    scrollLeft = await pagesContainer.evaluate((ele) => ele.scrollLeft);
+    expect(scrollLeft).toEqual(2753);
+
+    // Jump to the 7th page
+    await nextPageButton.click();
+    await page.waitForSelector('[data-testid="core__text-layer-6"]', { visible: true });
+    scrollLeft = await pagesContainer.evaluate((ele) => ele.scrollLeft);
+    expect(scrollLeft).toEqual(3161);
+
+    // Jump to the 8th page
+    await nextPageButton.click();
+    await page.waitForSelector('[data-testid="core__text-layer-7"]', { visible: true });
+    scrollLeft = await pagesContainer.evaluate((ele) => ele.scrollLeft);
+    expect(scrollLeft).toEqual(3161);
+});
+
+test('Document with different page dimensions (wrapped scroll mode)', async () => {
+    await page.goto('http://localhost:3000/default-layout-different-dimensions');
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+    });
+    await page.evaluate(() => document.querySelector('[data-testid="core__viewer"]').scrollIntoView());
+
+    // Wait until the first page is rendered
+    await page.waitForSelector('[data-testid="core__page-layer-0"]', { visible: true });
+
+    // Switch to the wrapped scroll mode
+    const moreActionsButton = await page.waitForSelector('[data-testid="toolbar__more-actions-popover-target"]');
+    await moreActionsButton.click();
+
+    const switchToWrappedMenu = await page.waitForSelector('[data-testid="scroll-mode__wrapped-menu"]');
+    await switchToWrappedMenu.click();
+
+    // Zoom to 50%
+    const zoomButton = await page.waitForSelector('[data-testid="zoom__popover-target"]');
+    await zoomButton.click();
+
+    const zoomPopover = await page.waitForSelector('[id="rpv-core__popover-body-inner-zoom"]');
+    const zoomMenuItem = await zoomPopover.waitForSelector('button:nth-of-type(4)');
+    const zoomLevel = await zoomMenuItem.evaluate((ele) => ele.textContent);
+    expect(zoomLevel).toEqual('50%');
+    await zoomMenuItem.click();
+
+    const pagesContainer = await page.waitForSelector('[data-testid="core__inner-pages"]');
+
+    await page.evaluate(() => document.querySelector('[data-testid="core__page-layer-2"]').scrollIntoView());
+    await page.waitForSelector('[data-testid="core__text-layer-1"]', { visible: true });
+    let scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(420);
+
+    await page.evaluate(() => document.querySelector('[data-testid="core__page-layer-5"]').scrollIntoView());
+    await page.waitForSelector('[data-testid="core__text-layer-5"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(832);
+
+    await page.evaluate(() => document.querySelector('[data-testid="core__page-layer-7"]').scrollIntoView());
+    await page.waitForSelector('[data-testid="core__text-layer-7"]', { visible: true });
+    scrollTop = await pagesContainer.evaluate((ele) => ele.scrollTop);
+    expect(scrollTop).toEqual(890);
+});
