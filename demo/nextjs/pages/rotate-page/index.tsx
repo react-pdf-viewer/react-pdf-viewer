ce087086a8afcad06d85dbfc7d830683c1926fba
Phuoc Nguyen <me@phuoc.ng>
2022-04-03 08:49:36 +0700

    feat: Add rotate a single page example using RotatePage component

---
+++ demo/nextjs/pages/rotate-page/index.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { PrimaryButton, Viewer } from '@react-pdf-viewer/core';
+import { rotatePlugin, RotateDirection } from '@react-pdf-viewer/rotate';
+
+const IndexPage = () => {
+    const rotatePluginInstance = rotatePlugin();
+    const { RotatePage } = rotatePluginInstance;
+
+    return (
+        <div
+            className="rpv-core__viewer"
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                flexDirection: 'column',
+                height: '50rem',
+                margin: '5rem auto',
+                width: '64rem',
+            }}
+        >
+            <div
+                style={{
+                    alignItems: 'center',
+                    backgroundColor: '#eeeeee',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    padding: '4px',
+                }}
+            >
+                <div style={{ padding: '0 0.25rem' }}>
+                    <RotatePage>
+                        {(props) => (
+                            <PrimaryButton onClick={() => props.onRotatePage(0, RotateDirection.Forward)}>
+                                Rotate the first page forward
+                            </PrimaryButton>
+                        )}
+                    </RotatePage>
+                </div>
+                <div style={{ padding: '0 0.25rem' }}>
+                    <RotatePage>
+                        {(props) => (
+                            <PrimaryButton onClick={() => props.onRotatePage(0, RotateDirection.Backward)}>
+                                Rotate the first page backward
+                            </PrimaryButton>
+                        )}
+                    </RotatePage>
+                </div>
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+                <Viewer defaultScale={0.5} fileUrl="/pdf-open-parameters.pdf" plugins={[rotatePluginInstance]} />
+            </div>
+        </div>
+    );
+};
+
+export default IndexPage;
