573aac500095a85a5c2cb23cc2fb7bcb9f68f38a
Phuoc Nguyen <me@phuoc.ng>
2022-04-03 21:27:18 +0700

    feat(test): Rotate single page using RotatePage component

---
+++ packages/rotate/__tests__/rotatePage.test.tsx
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import { fireEvent, render, waitForElementToBeRemoved } from '@testing-library/react';
+import { PrimaryButton, RotateDirection, Viewer } from '@react-pdf-viewer/core';
+
+import { mockIsIntersecting } from '../../../test-utils/mockIntersectionObserver';
+import { mockResize } from '../../../test-utils/mockResizeObserver';
+import { rotatePlugin } from '../src';
+
+const TestRotatePage: React.FC<{
+    fileUrl: Uint8Array;
+}> = ({ fileUrl }) => {
+    const rotatePluginInstance = rotatePlugin();
+    const { RotatePage } = rotatePluginInstance;
+
+    return (
+        <div
+            data-testid="root"
+            className="rpv-core__viewer"
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                flexDirection: 'column',
+                height: '50rem',
+                margin: '5rem auto',
+                width: '64rem',
+            }}
+        >
+            <div
+                style={{
+                    alignItems: 'center',
+                    backgroundColor: '#eeeeee',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    padding: '4px',
+                }}
+            >
+                <div style={{ padding: '0 0.25rem' }}>
+                    <RotatePage>
+                        {(props) => (
+                            <PrimaryButton
+                                testId="rotate-forward"
+                                onClick={() => props.onRotatePage(0, RotateDirection.Forward)}
+                            >
+                                Rotate the first page forward
+                            </PrimaryButton>
+                        )}
+                    </RotatePage>
+                </div>
+                <div style={{ padding: '0 0.25rem' }}>
+                    <RotatePage>
+                        {(props) => (
+                            <PrimaryButton
+                                testId="rotate-backward"
+                                onClick={() => props.onRotatePage(0, RotateDirection.Backward)}
+                            >
+                                Rotate the first page backward
+                            </PrimaryButton>
+                        )}
+                    </RotatePage>
+                </div>
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+                <Viewer defaultScale={0.5} fileUrl={fileUrl} plugins={[rotatePluginInstance]} />
+            </div>
+        </div>
+    );
+};
+
+test('Rotate single page using RotatePage component', async () => {
+    const { findByTestId, getByTestId } = render(<TestRotatePage fileUrl={global['__OPEN_PARAMS_PDF__']} />);
+
+    const viewerEle = getByTestId('core__viewer');
+    mockIsIntersecting(viewerEle, true);
+    viewerEle['__jsdomMockClientHeight'] = 757;
+    viewerEle['__jsdomMockClientWidth'] = 1022;
+
+    // Wait until the document is loaded completely
+    await waitForElementToBeRemoved(() => getByTestId('core__doc-loading'));
+
+    const pagesContainer = getByTestId('core__inner-pages');
+    pagesContainer.getBoundingClientRect = jest.fn(() => ({
+        x: 0,
+        y: 0,
+        height: 757,
+        width: 1022,
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        toJSON: () => {},
+    }));
+    mockResize(pagesContainer);
+
+    // Rotate forward the first page
+    const forwardBtn = getByTestId('rotate-forward');
+    fireEvent.click(forwardBtn);
+
+    // Check the size of first page
+    const pageLayer = await findByTestId('core__page-layer-0');
+    expect(pageLayer.style.height).toEqual('297px');
+    expect(pageLayer.style.width).toEqual('396px');
+
+    // Check if a text element is rotated
+    const textLayer = await findByTestId('core__text-layer-0');
+    const textElement = textLayer.childNodes[2] as HTMLElement;
+    expect(textElement.textContent).toEqual('Parameters for Opening PDF Files');
+    expect(textElement.style.left).toEqual('225.99px');
+    expect(textElement.style.top).toEqual('56.85px');
+});
