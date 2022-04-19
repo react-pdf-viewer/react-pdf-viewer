50d248abddf539dbaa772df4235bdbd422975c46
Phuoc Nguyen <me@phuoc.ng>
2022-04-19 20:36:35 +0700

    feat(demo): Set default scale to fit the page width

---
+++ demo/nextjs/pages/default-layout-fit-page-with/index.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { SpecialZoomLevel, Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+const IndexPage = () => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                height: '50rem',
+                margin: '5rem auto',
+                width: '64rem',
+            }}
+        >
+            <Viewer
+                defaultScale={SpecialZoomLevel.PageWidth}
+                fileUrl="/pdf-open-parameters.pdf"
+                plugins={[defaultLayoutPluginInstance]}
+            />
+        </div>
+    );
+};
+
+export default IndexPage;
