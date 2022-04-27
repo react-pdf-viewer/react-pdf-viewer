90557f1d60879c868b82399fc8edeb586b8a79f0
Phuoc Nguyen <me@phuoc.ng>
2022-04-27 08:41:08 +0700

    fix(test): Rotate single page using renderThumbnailItem

--- packages/thumbnail/__tests__/rotatePage.test.tsx
+++ packages/thumbnail/__tests__/rotatePage.test.tsx
@@ -112,7 +112,7 @@ test('Rotate single page using renderThumbnailItem', async () => {
     // Wait until the document is loaded completely
     await waitForElementToBeRemoved(() => getByTestId('core__doc-loading'));
 
-    const pagesContainer = getByTestId('core__inner-pages');
+    const pagesContainer = await findByTestId('core__inner-pages');
     pagesContainer.getBoundingClientRect = jest.fn(() => ({
         x: 0,
         y: 0,
