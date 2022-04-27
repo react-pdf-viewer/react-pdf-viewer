1553a531e164f7316b7e6c5057dcecaf2e02dc7f
Phuoc Nguyen <me@phuoc.ng>
2022-04-27 21:52:00 +0700

    fix(test): Test renderToolbar with <NumberOfPages />

--- packages/default-layout/__tests__/renderToolbarWithNumberOfPages.test.tsx
+++ packages/default-layout/__tests__/renderToolbarWithNumberOfPages.test.tsx
@@ -43,12 +43,15 @@ test('Test renderToolbar with <NumberOfPages />', async () => {
 
     // Wait until the document is loaded completely
     await waitForElementToBeRemoved(() => getByTestId('core__doc-loading'));
+    await findByTestId('core__text-layer-0');
+    await findByTestId('core__text-layer-1');
+    await findByTestId('core__text-layer-2');
 
     let pageLabel = await findByTestId('current-page-label');
     expect(pageLabel.textContent).toEqual('1 of 8');
 
     // Jump to the fourth page
-    const pagesContainer = getByTestId('core__inner-pages');
+    const pagesContainer = await findByTestId('core__inner-pages');
     pagesContainer.getBoundingClientRect = jest.fn(() => ({
         x: 0,
         y: 0,
@@ -68,7 +71,9 @@ test('Test renderToolbar with <NumberOfPages />', async () => {
         },
     });
 
-    const page = await findByTestId('core__page-layer-3');
+    await findByTestId('core__text-layer-3');
+    await findByTestId('core__text-layer-4');
+
     pageLabel = await findByTestId('current-page-label');
     expect(pageLabel.textContent).toEqual('4 of 8');
 });
