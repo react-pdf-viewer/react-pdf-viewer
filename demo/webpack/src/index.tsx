b39c48d6a940c16f21873a154017094c0e8c6c5b
Phuoc Nguyen <me@phuoc.ng>
2022-04-28 20:18:46 +0700

    fix: Use createRoot introduced in React 18

--- demo/webpack/src/index.tsx
+++ demo/webpack/src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Worker } from '@react-pdf-viewer/core';
 
 import App from './App';
 
 const rootElement = document.getElementById('root');
-render(
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.13.216/build/pdf.worker.js">
-        <App />
-    </Worker>,
-    rootElement
+const root = createRoot(rootElement);
+root.render(
+    <React.StrictMode>
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.13.216/build/pdf.worker.js">
+            <App />
+        </Worker>
+    </React.StrictMode>
 );
