32feac62e68e69e618c09565cb870e3f24ab1fad
Phuoc Nguyen <me@phuoc.ng>
2022-04-30 09:31:17 +0700

    fix(test): Keep highlighting after clicking zoom buttons in the default toolbar

--- packages/search/__tests__/keepHighlightAfterZoomToolbar.test.tsx
+++ packages/search/__tests__/keepHighlightAfterZoomToolbar.test.tsx
@@ -62,8 +62,13 @@ test('Keep highlighting after clicking zoom buttons in the default toolbar', asy
     // Wait until the document is loaded completely
     await waitForElementToBeRemoved(() => screen.getByTestId('core__doc-loading'));
     await findByTestId('core__text-layer-0');
+    await findByTestId('core__annotation-layer-0');
     await findByTestId('core__text-layer-1');
+    await findByTestId('core__annotation-layer-1');
     await findByTestId('core__text-layer-2');
+    await findByTestId('core__annotation-layer-2');
+    await findByTestId('core__text-layer-3');
+    await findByTestId('core__annotation-layer-3');
 
     const pagesContainer = await findByTestId('core__inner-pages');
     pagesContainer.getBoundingClientRect = jest.fn(() => ({
@@ -85,10 +90,16 @@ test('Keep highlighting after clicking zoom buttons in the default toolbar', asy
         },
     });
 
+    await findByTestId('core__text-layer-4');
+    await findByTestId('core__annotation-layer-4');
+    await findByTestId('core__text-layer-5');
+    await findByTestId('core__annotation-layer-5');
+
     let page = await findByTestId('core__page-layer-6');
 
     // Wait for the text layer to be rendered completely
     await findByTestId('core__text-layer-6');
+    await findByTestId('core__annotation-layer-6');
 
     let highlights = await findAllByTitle(page, keyword);
     expect(highlights.length).toEqual(3);
