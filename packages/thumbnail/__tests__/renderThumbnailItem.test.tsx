da3c67ac87d817c4120075477837a2ee9d21e061
Phuoc Nguyen <me@phuoc.ng>
2022-04-30 09:32:29 +0700

    fix(test): Test renderThumbnailItem option

--- packages/thumbnail/__tests__/renderThumbnailItem.test.tsx
+++ packages/thumbnail/__tests__/renderThumbnailItem.test.tsx
@@ -68,6 +68,14 @@ test('Test renderThumbnailItem option', async () => {
 
     // Wait until the document is loaded completely
     await waitForElementToBeRemoved(() => screen.getByTestId('core__doc-loading'));
+    await findByTestId('core__text-layer-0');
+    await findByTestId('core__annotation-layer-0');
+    await findByTestId('core__text-layer-1');
+    await findByTestId('core__annotation-layer-1');
+    await findByTestId('core__text-layer-2');
+    await findByTestId('core__annotation-layer-2');
+    await findByTestId('core__text-layer-3');
+    await findByTestId('core__annotation-layer-3');
 
     const pagesContainer = await findByTestId('core__inner-pages');
     pagesContainer.getBoundingClientRect = jest.fn(() => ({
