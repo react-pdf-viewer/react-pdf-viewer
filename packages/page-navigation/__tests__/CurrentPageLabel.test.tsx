e58fa63049ad1278f9eb544f3a908a5eeed9a4ab
Phuoc Nguyen <me@phuoc.ng>
2022-04-30 09:30:15 +0700

    fix(test): Test <CurrentPageLabel> with custom page label

--- packages/page-navigation/__tests__/CurrentPageLabel.test.tsx
+++ packages/page-navigation/__tests__/CurrentPageLabel.test.tsx
@@ -62,8 +62,13 @@ test('Test <CurrentPageLabel>', async () => {
     // Wait until the document is loaded completely
     await waitForElementToBeRemoved(() => getByTestId('core__doc-loading'));
     await findByTestId('core__text-layer-0');
+    await findByTestId('core__annotation-layer-0');
     await findByTestId('core__text-layer-1');
+    await findByTestId('core__annotation-layer-1');
     await findByTestId('core__text-layer-2');
+    await findByTestId('core__annotation-layer-2');
+    await findByTestId('core__text-layer-3');
+    await findByTestId('core__annotation-layer-3');
 
     let pageLabel = await findByTestId('current-page-label');
     expect(pageLabel.textContent).toEqual('8');
@@ -108,8 +113,13 @@ test('Test <CurrentPageLabel> with custom page label', async () => {
     // Wait until the document is loaded completely
     await waitForElementToBeRemoved(() => getByTestId('core__doc-loading'));
     await findByTestId('core__text-layer-0');
+    await findByTestId('core__annotation-layer-0');
     await findByTestId('core__text-layer-1');
+    await findByTestId('core__annotation-layer-1');
     await findByTestId('core__text-layer-2');
+    await findByTestId('core__annotation-layer-2');
+    await findByTestId('core__text-layer-3');
+    await findByTestId('core__annotation-layer-3');
 
     let pageLabel = await findByTestId('current-page-label');
     expect(pageLabel.textContent).toEqual('8(296)');
@@ -134,6 +144,9 @@ test('Test <CurrentPageLabel> with custom page label', async () => {
             scrollTop: 1782,
         },
     });
+
+    await findByTestId('core__text-layer-4');
+    await findByTestId('core__annotation-layer-4');
 
     await findByTestId('core__text-layer-3');
     pageLabel = await findByTestId('current-page-label');
