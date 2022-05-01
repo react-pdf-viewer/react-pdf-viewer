6fd8ad188e89c99740de490c7f81447d3253bdc9
Phuoc Nguyen <me@phuoc.ng>
2022-05-01 08:45:09 +0700

    feat(demo): Example of setting thumbnail width

---
+++ demo/nextjs/pages/thumbnail-width/index.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import { thumbnailPlugin } from '@react-pdf-viewer/thumbnail';
+
+const IndexPage = () => {
+    const thumbnailPluginInstance = thumbnailPlugin({
+        thumbnailWidth: 150,
+    });
+    const { Thumbnails } = thumbnailPluginInstance;
+
+    return (
+        <div
+            data-testid="root"
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.1)',
+                display: 'flex',
+                height: '50rem',
+                margin: '1rem auto',
+                width: '64rem',
+            }}
+        >
+            <div
+                style={{
+                    borderRight: '1px solid rgba(0, 0, 0, 0.1)',
+                    width: '20%',
+                }}
+            >
+                <Thumbnails />
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+                <Viewer fileUrl="/pdf-open-parameters.pdf" plugins={[thumbnailPluginInstance]} />
+            </div>
+        </div>
+    );
+};
+
+export default IndexPage;
