7a690c9e2cbf738b712c5e5ba19fb4ec47ff5e30
Phuoc Nguyen <me@phuoc.ng>
2022-05-02 16:41:57 +0700

    feat(e2e): Support Strict mode

---
+++ packages/default-layout/__tests__/strictMode.e2e.ts
@@ -0,0 +1,36 @@
+import 'expect-puppeteer';
+
+test('Support Strict mode', async () => {
+    await page.goto('http://localhost:3000/default-layout-strict-mode');
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+    });
+    await page.evaluate(() => document.querySelector('[data-testid="core__viewer"]').scrollIntoView());
+
+    // Wait until the first page is rendered
+    const firstPage = await page.waitForSelector('[data-testid="core__text-layer-0"]', { visible: true });
+
+    const textElements = await firstPage.$$('.rpv-core__text-layer-text');
+    expect(textElements.length).toEqual(16);
+
+    // Click the `Thumbnail` tab in the sidebar
+    const thumbnailTab = await page.waitForSelector('button[aria-label="Thumbnail"]');
+    await thumbnailTab.click();
+
+    // To make sure the thumbnails are rendered
+    await page.waitForSelector('[data-testid="thumbnail__list"]');
+
+    const firstThumbnail = await page.waitForSelector('[aria-label="Thumbnail of page 1"]');
+    let props = await firstThumbnail.evaluate((ele) => ({
+        height: ele.getAttribute('height'),
+        width: ele.getAttribute('width'),
+        src: ele.getAttribute('src'),
+    }));
+    expect(props.src.length).toEqual(3702);
+    expect(props.src.substring(0, 100)).toEqual(
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAACFCAYAAACt+l1zAAAAAXNSR0IArs4c6QAACoJJREFUeF7tnX'
+    );
+    expect(props.width).toEqual('100px');
+    expect(props.height).toEqual('133.33333333333334px');
+});
