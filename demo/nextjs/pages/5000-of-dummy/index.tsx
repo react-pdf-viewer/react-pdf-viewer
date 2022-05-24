426b68931be5f476bf1f3e78e1c09091b1df9b13
Phuoc Nguyen <me@phuoc.ng>
2022-05-24 21:59:18 +0700

    feat(demo): Add example of using a big document

---
+++ demo/nextjs/pages/5000-of-dummy/index.tsx
@@ -0,0 +1,61 @@
+import type { RenderPageProps } from '@react-pdf-viewer/core';
+import { Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import * as React from 'react';
+
+const IndexPage = () => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+    const renderPage = (props: RenderPageProps) => (
+        <>
+            {props.canvasLayer.children}
+            <div
+                style={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    height: '100%',
+                    justifyContent: 'center',
+                    left: 0,
+                    position: 'absolute',
+                    top: 0,
+                    width: '100%',
+                }}
+            >
+                <div
+                    style={{
+                        color: 'rgba(0, 0, 0, 0.2)',
+                        fontSize: `${8 * props.scale}rem`,
+                        fontWeight: 'bold',
+                        textTransform: 'uppercase',
+                        transform: 'rotate(-45deg)',
+                        userSelect: 'none',
+                    }}
+                >
+                    {props.pageIndex + 1}
+                </div>
+            </div>
+            {props.annotationLayer.children}
+            {props.textLayer.children}
+        </>
+    );
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                height: '50rem',
+                margin: '5rem auto',
+                width: '64rem',
+            }}
+        >
+            <Viewer
+                fileUrl="/5000-of-dummy-optimized.pdf"
+                initialPage={100}
+                plugins={[defaultLayoutPluginInstance]}
+                renderPage={renderPage}
+            />
+        </div>
+    );
+};
+
+export default IndexPage;
