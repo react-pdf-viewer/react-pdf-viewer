8f6d15aff0aefab3c034af72f2702fd6467de488
Phuoc Nguyen <me@phuoc.ng>
2022-05-30 22:48:11 +0700

    fix(test): Thumbnails are not displayed when switching between tabs

--- packages/default-layout/__tests__/Thumbnails.test.tsx
+++ packages/default-layout/__tests__/Thumbnails.test.tsx
@@ -1,5 +1,5 @@
 import { Viewer } from '@react-pdf-viewer/core';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, waitForElementToBeRemoved } from '@testing-library/react';
 import * as React from 'react';
 import { mockIsIntersecting } from '../../../test-utils/mockIntersectionObserver';
 import { defaultLayoutPlugin } from '../src';
@@ -23,6 +23,18 @@ test('Thumbnails are not displayed when switching between tabs', async () => {
 
     const viewerEle = getByTestId('core__viewer');
     mockIsIntersecting(viewerEle, true);
+    viewerEle['__jsdomMockClientHeight'] = 800;
+    viewerEle['__jsdomMockClientWidth'] = 800;
+
+    await waitForElementToBeRemoved(() => getByTestId('core__doc-loading'));
+    await findByTestId('core__text-layer-0');
+    await findByTestId('core__annotation-layer-0');
+    await findByTestId('core__text-layer-1');
+    await findByTestId('core__annotation-layer-1');
+    await findByTestId('core__text-layer-2');
+    await findByTestId('core__annotation-layer-2');
+    await findByTestId('core__text-layer-3');
+    await findByTestId('core__annotation-layer-3');
 
     // Click the `Thumbnail` tab
     const thumbnailTab = await findByLabelText('Thumbnail');
@@ -39,9 +51,9 @@ test('Thumbnails are not displayed when switching between tabs', async () => {
     let firstThumbnail = await findByLabelText('Thumbnail of page 1');
     let firstThumbnailSrc = firstThumbnail.getAttribute('src');
     expect(firstThumbnailSrc.substring(0, 100)).toEqual(
-        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAACFCAYAAACt+l1zAAAABmJLR0QA/wD/AP+gvaeTAAAKgUlEQV'
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAACFCAYAAACt+l1zAAAABmJLR0QA/wD/AP+gvaeTAAAKX0lEQV'
     );
-    expect(firstThumbnailSrc.length).toEqual(3710);
+    expect(firstThumbnailSrc.length).toEqual(3662);
 
     // Click the `Bookmark` tab
     const bookmarkTab = await findByLabelText('Bookmark');
@@ -63,7 +75,7 @@ test('Thumbnails are not displayed when switching between tabs', async () => {
     firstThumbnail = await findByLabelText('Thumbnail of page 1');
     firstThumbnailSrc = firstThumbnail.getAttribute('src');
     expect(firstThumbnailSrc.substring(0, 100)).toEqual(
-        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAACFCAYAAACt+l1zAAAABmJLR0QA/wD/AP+gvaeTAAAKgUlEQV'
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAACFCAYAAACt+l1zAAAABmJLR0QA/wD/AP+gvaeTAAAKX0lEQV'
     );
-    expect(firstThumbnailSrc.length).toEqual(3710);
+    expect(firstThumbnailSrc.length).toEqual(3662);
 });
