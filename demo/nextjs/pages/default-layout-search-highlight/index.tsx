01d304133ae7b55102e9e7ef63184ab7ddfac722
Phuoc Nguyen <me@phuoc.ng>
2022-06-14 20:53:33 +0700

    feat(demo): Use search keyword and highlight plugin

---
+++ demo/nextjs/pages/default-layout-search-highlight/index.tsx
@@ -0,0 +1,228 @@
+import { Button, DocumentLoadEvent, PdfJs, Position, PrimaryButton, Tooltip, Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import {
+    HighlightArea,
+    highlightPlugin,
+    MessageIcon,
+    RenderHighlightContentProps,
+    RenderHighlightsProps,
+    RenderHighlightTargetProps,
+} from '@react-pdf-viewer/highlight';
+import * as React from 'react';
+
+interface Note {
+    id: number;
+    content: string;
+    highlightAreas: HighlightArea[];
+    quote: string;
+}
+
+const IndexPage = () => {
+    const [message, setMessage] = React.useState('');
+    const [notes, setNotes] = React.useState<Note[]>([]);
+    const notesContainerRef = React.useRef<HTMLDivElement | null>(null);
+    let noteId = notes.length;
+
+    const noteEles: Map<number, HTMLElement> = new Map();
+    const [currentDoc, setCurrentDoc] = React.useState<PdfJs.PdfDocument | null>(null);
+
+    const handleDocumentLoad = (e: DocumentLoadEvent) => {
+        setCurrentDoc(e.doc);
+        if (currentDoc && currentDoc !== e.doc) {
+            // User opens new document
+            setNotes([]);
+        }
+    };
+
+    const renderHighlightTarget = (props: RenderHighlightTargetProps) => (
+        <div
+            style={{
+                background: '#eee',
+                display: 'flex',
+                position: 'absolute',
+                left: `${props.selectionRegion.left}%`,
+                top: `${props.selectionRegion.top + props.selectionRegion.height}%`,
+                transform: 'translate(0, 8px)',
+                zIndex: 1,
+            }}
+        >
+            <Tooltip
+                position={Position.TopCenter}
+                target={
+                    <Button onClick={props.toggle}>
+                        <MessageIcon />
+                    </Button>
+                }
+                content={() => <div style={{ width: '100px' }}>Add a note</div>}
+                offset={{ left: 0, top: -8 }}
+            />
+        </div>
+    );
+
+    const renderHighlightContent = (props: RenderHighlightContentProps) => {
+        const addNote = () => {
+            if (message !== '') {
+                const note: Note = {
+                    id: ++noteId,
+                    content: message,
+                    highlightAreas: props.highlightAreas,
+                    quote: props.selectedText,
+                };
+                setNotes(notes.concat([note]));
+                props.cancel();
+            }
+        };
+
+        return (
+            <div
+                style={{
+                    background: '#fff',
+                    border: '1px solid rgba(0, 0, 0, .3)',
+                    borderRadius: '2px',
+                    padding: '8px',
+                    position: 'absolute',
+                    left: `${props.selectionRegion.left}%`,
+                    top: `${props.selectionRegion.top + props.selectionRegion.height}%`,
+                    zIndex: 1,
+                }}
+            >
+                <div>
+                    <textarea
+                        rows={3}
+                        style={{
+                            border: '1px solid rgba(0, 0, 0, .3)',
+                        }}
+                        onChange={(e) => setMessage(e.target.value)}
+                    ></textarea>
+                </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        marginTop: '8px',
+                    }}
+                >
+                    <div style={{ marginRight: '8px' }}>
+                        <PrimaryButton onClick={addNote}>Add</PrimaryButton>
+                    </div>
+                    <Button onClick={props.cancel}>Cancel</Button>
+                </div>
+            </div>
+        );
+    };
+
+    const jumpToNote = (note: Note) => {
+        activateTab(3);
+        const notesContainer = notesContainerRef.current;
+        if (noteEles.has(note.id) && notesContainer) {
+            notesContainer.scrollTop = noteEles.get(note.id).getBoundingClientRect().top;
+        }
+    };
+
+    const renderHighlights = (props: RenderHighlightsProps) => (
+        <div>
+            {notes.map((note) => (
+                <React.Fragment key={note.id}>
+                    {note.highlightAreas
+                        .filter((area) => area.pageIndex === props.pageIndex)
+                        .map((area, idx) => (
+                            <div
+                                key={idx}
+                                style={Object.assign(
+                                    {},
+                                    {
+                                        background: 'yellow',
+                                        opacity: 0.4,
+                                    },
+                                    props.getCssProperties(area, props.rotation)
+                                )}
+                                onClick={() => jumpToNote(note)}
+                            />
+                        ))}
+                </React.Fragment>
+            ))}
+        </div>
+    );
+
+    const highlightPluginInstance = highlightPlugin({
+        renderHighlightTarget,
+        renderHighlightContent,
+        renderHighlights,
+    });
+
+    const { jumpToHighlightArea } = highlightPluginInstance;
+
+    React.useEffect(() => {
+        return () => {
+            noteEles.clear();
+        };
+    }, []);
+
+    const sidebarNotes = (
+        <div
+            ref={notesContainerRef}
+            style={{
+                overflow: 'auto',
+                width: '100%',
+            }}
+        >
+            {notes.length === 0 && <div style={{ textAlign: 'center' }}>There is no note</div>}
+            {notes.map((note) => {
+                return (
+                    <div
+                        key={note.id}
+                        style={{
+                            borderBottom: '1px solid rgba(0, 0, 0, .3)',
+                            cursor: 'pointer',
+                            padding: '8px',
+                        }}
+                        onClick={() => jumpToHighlightArea(note.highlightAreas[0])}
+                        ref={(ref): void => {
+                            noteEles.set(note.id, ref as HTMLElement);
+                        }}
+                    >
+                        <blockquote
+                            style={{
+                                borderLeft: '2px solid rgba(0, 0, 0, 0.2)',
+                                fontSize: '.75rem',
+                                lineHeight: 1.5,
+                                margin: '0 0 8px 0',
+                                paddingLeft: '8px',
+                                textAlign: 'justify',
+                            }}
+                        >
+                            {note.quote}
+                        </blockquote>
+                        {note.content}
+                    </div>
+                );
+            })}
+        </div>
+    );
+
+    const defaultLayoutPluginInstance = defaultLayoutPlugin({
+        toolbarPlugin: {
+            searchPlugin: {
+                keyword: 'document',
+            },
+        },
+        sidebarTabs: (defaultTabs) =>
+            defaultTabs.concat({
+                content: sidebarNotes,
+                icon: <MessageIcon />,
+                title: 'Notes',
+            }),
+    });
+    const { activateTab } = defaultLayoutPluginInstance;
+
+    return (
+        <div className="demo">
+            <Viewer
+                fileUrl="/pdf-open-parameters.pdf"
+                plugins={[highlightPluginInstance, defaultLayoutPluginInstance]}
+                onDocumentLoad={handleDocumentLoad}
+            />
+        </div>
+    );
+};
+
+export default IndexPage;
