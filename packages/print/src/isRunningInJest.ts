af4032e83811e18b99ea29708bd7c536dcb99d53
Phuoc Nguyen <me@phuoc.ng>
2022-06-18 16:43:50 +0700

    fix: Check if the code is running in Jest

--- packages/print/src/isRunningInJest.ts
+++ packages/print/src/isRunningInJest.ts
@@ -7,4 +7,4 @@
  */
 
 // Returns `true` if the code is running in Jest environment
-export const isRunningInJest = () => process.env.JEST_WORKER_ID !== undefined;
+export const isRunningInJest = () => typeof process !== 'undefined' && process.env.JEST_WORKER_ID !== undefined;
