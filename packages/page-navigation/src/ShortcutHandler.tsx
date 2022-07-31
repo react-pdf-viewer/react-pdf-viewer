331dff5e347ffa5ab7663fc3e27a52c022f6382b
Phuoc Nguyen <me@phuoc.ng>
2022-07-31 17:19:49 +0700

    feat: Bring users to the previous clicked link annotation

--- packages/page-navigation/src/ShortcutHandler.tsx
+++ packages/page-navigation/src/ShortcutHandler.tsx
@@ -6,16 +6,26 @@
  * @copyright 2019-2022 Nguyen Huu Phuoc <me@phuoc.ng>
  */
 
-import type { Store } from '@react-pdf-viewer/core';
+import type { PdfJs, Store } from '@react-pdf-viewer/core';
+import { isMac } from '@react-pdf-viewer/core';
 import * as React from 'react';
-import type { StoreProps } from './types/StoreProps';
+import type { JumpFromAnnotation, StoreProps } from './types/StoreProps';
 import { useCurrentPage } from './useCurrentPage';
 
 export const ShortcutHandler: React.FC<{
     containerRef: React.RefObject<HTMLDivElement>;
+    doc: PdfJs.PdfDocument;
     numPages: number;
     store: Store<StoreProps>;
-}> = ({ containerRef, numPages, store }) => {
+}> = ({ containerRef, doc, numPages, store }) => {
+    const jumpFromAnnotationRef = React.useRef<JumpFromAnnotation>(
+        store.get('jumpFromAnnotation') || {
+            bottomOffset: 0,
+            dest: '',
+            leftOffset: 0,
+            pageIndex: -1,
+        }
+    );
     const { currentPage } = useCurrentPage(store);
     const currentPageRef = React.useRef(currentPage);
     currentPageRef.current = currentPage;
@@ -47,6 +57,14 @@ export const ShortcutHandler: React.FC<{
         }
     };
 
+    const jumpToAnnotation = (target: JumpFromAnnotation) => {
+        const jumpToDestination = store.get('jumpToDestination');
+        if (jumpToDestination) {
+            const { pageIndex, bottomOffset, leftOffset } = target;
+            jumpToDestination(pageIndex, bottomOffset, leftOffset);
+        }
+    };
+
     const handleKeydown = (e: KeyboardEvent) => {
         const containerEle = containerRef.current;
         const shouldHandleShortcuts =
@@ -60,14 +78,32 @@ export const ShortcutHandler: React.FC<{
             (e.altKey && e.key === 'ArrowDown') || (!e.shiftKey && !e.altKey && e.key === 'PageDown');
         const shouldGoToPreviousPage =
             (e.altKey && e.key === 'ArrowUp') || (!e.shiftKey && !e.altKey && e.key === 'PageUp');
+
+        const isCommandPressed = isMac() ? e.metaKey && !e.ctrlKey : e.ctrlKey;
+        const shouldJumpBackAnnotation = isCommandPressed && e.key === 'ArrowUp';
         if (shouldGoToNextPage) {
             e.preventDefault();
             goToNextPage();
         } else if (shouldGoToPreviousPage) {
             e.preventDefault();
             goToPreviousPage();
+        } else if (shouldJumpBackAnnotation && jumpFromAnnotationRef.current && jumpFromAnnotationRef.current.dest) {
+            e.preventDefault();
+            jumpToAnnotation(jumpFromAnnotationRef.current);
         }
     };
+
+    const handleJumpFromAnnotationChanged = (target: JumpFromAnnotation) => {
+        jumpFromAnnotationRef.current = target;
+    };
+
+    React.useEffect(() => {
+        store.subscribe('jumpFromAnnotation', handleJumpFromAnnotationChanged);
+
+        return () => {
+            store.unsubscribe('jumpFromAnnotation', handleJumpFromAnnotationChanged);
+        };
+    }, []);
 
     React.useEffect(() => {
         const containerEle = containerRef.current;
