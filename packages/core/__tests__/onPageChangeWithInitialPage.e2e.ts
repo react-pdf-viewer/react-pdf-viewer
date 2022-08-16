cb7eb8f064b8dee55b4940baf976af136b69d562
Phuoc Nguyen <me@phuoc.ng>
2022-08-16 22:42:10 +0700

    feat(e2e): onPageChange should trigger when current page equals initial page

--- packages/core/__tests__/onPageChangeWithInitialPage.e2e.ts
+++ packages/core/__tests__/onPageChangeWithInitialPage.e2e.ts
@@ -14,4 +14,23 @@ test('Test the onPageChange() callback with initial page', async () => {
     let visitedPagesLabel = await page.waitForSelector('[data-testid="visited-pages"]');
     let visitedPages = await visitedPagesLabel?.evaluate((ele) => ele.textContent);
     expect(visitedPages).toEqual('5');
+
+    // Issue 1197 (https://github.com/react-pdf-viewer/react-pdf-viewer/issues/1197)
+    // Scroll to the 6th page
+    await page.evaluate(() => document.querySelector('[data-testid="core__page-layer-6"]')?.scrollIntoView());
+    await page.waitForSelector('[data-testid="core__text-layer-6"]', { visible: true });
+    await page.waitForSelector('[data-testid="core__annotation-layer-6"]', { visible: true });
+    await page.waitForFunction(() => 'document.querySelector("[data-testid=visited-pages]").textContent === "5, 6"');
+
+    // Scroll to the 7th page
+    await page.evaluate(() => document.querySelector('[data-testid="core__page-layer-7"]')?.scrollIntoView());
+    await page.waitForSelector('[data-testid="core__text-layer-7"]', { visible: true });
+    await page.waitForSelector('[data-testid="core__annotation-layer-7"]', { visible: true });
+    await page.waitForFunction(() => 'document.querySelector("[data-testid=visited-pages]").textContent === "5, 6, 7"');
+
+    // Jump back to the 5th page
+    await page.evaluate(() => document.querySelector('[data-testid="core__page-layer-5"]')?.scrollIntoView());
+    await page.waitForFunction(
+        () => 'document.querySelector("[data-testid=visited-pages]").textContent === "5, 6, 7, 5"'
+    );
 });
