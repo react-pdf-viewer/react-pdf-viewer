180087159896bef8ebaf87b2813d9ef9773ead83
Phuoc Nguyen <me@phuoc.ng>
2022-10-15 17:14:02 +0700

    feat: Update current value of store

--- packages/core/src/types/Store.ts
+++ packages/core/src/types/Store.ts
@@ -16,5 +16,6 @@ export interface Store<T extends StoreState> {
     subscribe<K extends StoreKey<T>>(eventName: K, handler: StoreHandler<NonNullable<T[K]>>): void;
     unsubscribe<K extends StoreKey<T>>(eventName: K, handler: StoreHandler<NonNullable<T[K]>>): void;
     update<K extends StoreKey<T>>(eventName: K, params: T[K]): void;
+    updateCurrentValue<K extends StoreKey<T>>(eventName: K, updater: (currentValue: T[K]) => T[K]): void;
     get<K extends StoreKey<T>>(eventName: K): T[K] | undefined;
 }
