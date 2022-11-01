7dbb5d34524c46e3a2fd542d33a9905bcc3ea199
Phuoc Nguyen <me@phuoc.ng>
2022-11-01 20:08:00 +0700

    feat: Add initial keyword demo

---
+++ demo/nextjs/pages/default-layout-initial-keyword/index.tsx
@@ -0,0 +1,31 @@
+import { Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import * as React from 'react';
+
+const IndexPage = () => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin({
+        toolbarPlugin: {
+            searchPlugin: {
+                keyword: {
+                    keyword: 'PDF',
+                    matchCase: true,
+                },
+            },
+        },
+    });
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                height: '50rem',
+                margin: '5rem auto',
+                width: '64rem',
+            }}
+        >
+            <Viewer fileUrl="/pdf-open-parameters.pdf" plugins={[defaultLayoutPluginInstance]} />
+        </div>
+    );
+};
+
+export default IndexPage;
