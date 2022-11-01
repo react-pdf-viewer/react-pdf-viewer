84590552939a348ea531c2dba5b48ce1148c7690
Phuoc Nguyen <me@phuoc.ng>
2022-11-01 22:17:09 +0700

    feat: Use the current Chrome

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     launch: {
+        // Use the current Chrome, so e2e tests give the same results as local development
+        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
         // We need it for full-screen plugin test cases
         headless: false,
         product: 'chrome',
