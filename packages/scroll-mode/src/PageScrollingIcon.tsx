d7acc2fb66216746ef495e8a66068770761dbb64
Phuoc Nguyen <me@phuoc.ng>
2022-12-17 09:51:53 +0700

    feat: New Page Scrolling icon

---
+++ packages/scroll-mode/src/PageScrollingIcon.tsx
@@ -0,0 +1,16 @@
+/**
+ * A React component to view a PDF document
+ *
+ * @see https://react-pdf-viewer.dev
+ * @license https://react-pdf-viewer.dev/license
+ * @copyright 2019-2022 Nguyen Huu Phuoc <me@phuoc.ng>
+ */
+
+import { Icon } from '@react-pdf-viewer/core';
+import * as React from 'react';
+
+export const PageScrollingIcon: React.FC = () => (
+    <Icon size={16}>
+       <rect x='0.5' y='0.497' width='22' height='22' rx='1' ry='1' />
+    </Icon>
+);
