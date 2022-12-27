239637ddce3bbfdf9e22977e837aaede162ccbd6
Phuoc Nguyen <me@phuoc.ng>
2022-12-27 18:52:29 +0700

    fix(test): Remove the sidebar

--- packages/default-layout/__tests__/removeSidebar.test.tsx
+++ packages/default-layout/__tests__/removeSidebar.test.tsx
@@ -1,5 +1,5 @@
 import { TextDirection, Viewer } from '@react-pdf-viewer/core';
-import { render } from '@testing-library/react';
+import { render, waitForElementToBeRemoved } from '@testing-library/react';
 import * as React from 'react';
 import { mockIsIntersecting } from '../../../test-utils/mockIntersectionObserver';
 import { defaultLayoutPlugin } from '../src';
@@ -31,6 +31,18 @@ test('Remove the sidebar', async () => {
 
     const viewerEle = getByTestId('core__viewer');
     mockIsIntersecting(viewerEle, true);
+    viewerEle['__jsdomMockClientHeight'] = 720;
+    viewerEle['__jsdomMockClientWidth'] = 720;
+
+    await waitForElementToBeRemoved(() => getByTestId('core__doc-loading'));
+    await findByTestId('core__text-layer-0');
+    await findByTestId('core__annotation-layer-0');
+    await findByTestId('core__text-layer-1');
+    await findByTestId('core__annotation-layer-1');
+    await findByTestId('core__text-layer-2');
+    await findByTestId('core__annotation-layer-2');
+    await findByTestId('core__text-layer-3');
+    await findByTestId('core__annotation-layer-3');
 
     await findByTestId('default-layout__main');
 
