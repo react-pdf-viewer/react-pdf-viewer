23328883426b68526eff1e10ac1d46a47052ee88
Phuoc Nguyen <me@phuoc.ng>
2023-01-15 18:35:30 +0700

    fix: Remove unused visibilities tracking

--- packages/core/src/layouts/Inner.tsx
+++ packages/core/src/layouts/Inner.tsx
@@ -126,10 +126,6 @@ export const Inner: React.FC<{
     const [scale, setScale] = React.useState(initialScale);
     const previousScale = usePrevious(scale);
 
-    // Track visibilities of pages
-    const defaultVisibilities = React.useMemo(() => Array(numPages).fill(-1) as number[], []);
-    const [visibilities, setVisibilities] = React.useState(defaultVisibilities);
-
     const stateRef = React.useRef<ViewerState>(viewerState);
     const keepSpecialZoomLevelRef = React.useRef<SpecialZoomLevel | null>(
         typeof defaultScale === 'string' ? defaultScale : null
