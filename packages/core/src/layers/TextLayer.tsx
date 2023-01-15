3c75a9e5addd84a1651fd1d402a2504869ba34ed
Phuoc Nguyen <me@phuoc.ng>
2023-01-15 17:53:42 +0700

    fix: Replace textContent with textContentSource

--- packages/core/src/layers/TextLayer.tsx
+++ packages/core/src/layers/TextLayer.tsx
@@ -65,7 +65,10 @@ export const TextLayer: React.FC<{
             empty();
             renderTask.current = PdfJsApi.renderTextLayer({
                 container: containerEle,
+                // From pdf-js 3.2.146, the `textContent` parameter is deprecated 
+                // and will be soon replaced with the `textContentSource` parameter
                 textContent: textContent as any,
+                textContentSource: textContent as any,
                 viewport: viewport as any,
             });
             renderTask.current.promise.then(
