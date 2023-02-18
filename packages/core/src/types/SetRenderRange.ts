708aefedbb47bd8cbf19c81f42efac8129a1d484
Phuoc Nguyen <me@phuoc.ng>
2023-02-18 11:32:54 +0700

    ref: Combine functions to define the pages range

---
+++ packages/core/src/types/SetRenderRange.ts
@@ -0,0 +1,15 @@
+/**
+ * A React component to view a PDF document
+ *
+ * @see https://react-pdf-viewer.dev
+ * @license https://react-pdf-viewer.dev/license
+ * @copyright 2019-2023 Nguyen Huu Phuoc <me@phuoc.ng>
+ */
+
+export interface VisiblePagesRange {
+    endPage: number;
+    numPages: number;
+    startPage: number;
+}
+
+export type SetRenderRange = (visiblePagesRange: VisiblePagesRange) => { endPage: number; startPage: number };
