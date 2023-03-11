25d2251ceda7409d3507a46e4a5b545b88358a15
Phuoc Nguyen <me@phuoc.ng>
2023-03-11 23:31:31 +0700

    feat(demo): Update full-screen example

--- demo/nextjs/pages/full-screen-switch-scroll-mode/index.tsx
+++ demo/nextjs/pages/full-screen-switch-scroll-mode/index.tsx
@@ -1,4 +1,5 @@
-import { ScrollMode, Viewer } from '@react-pdf-viewer/core';
+import type { RenderPage, RenderPageProps } from '@react-pdf-viewer/core';
+import { ScrollMode, Viewer, ViewMode } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
 const IndexPage = () => {
@@ -19,19 +20,55 @@ const IndexPage = () => {
         },
     });
 
+    const renderPage: RenderPage = (props: RenderPageProps) => (
+        <>
+            {props.canvasLayer.children}
+            <div
+                style={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    height: '100%',
+                    justifyContent: 'center',
+                    left: 0,
+                    position: 'absolute',
+                    top: 0,
+                    width: '100%',
+                    backgroundColor: '#fff',
+                }}
+            >
+                <div
+                    style={{
+                        color: 'rgba(0, 0, 0, 0.2)',
+                        fontSize: `${8 * props.scale}rem`,
+                        fontWeight: 'bold',
+                        textTransform: 'uppercase',
+                        userSelect: 'none',
+                    }}
+                >
+                    {props.pageIndex + 1}
+                </div>
+            </div>
+            {props.annotationLayer.children}
+            {props.textLayer.children}
+        </>
+    );
+
     return (
         <div
             style={{
                 height: '50rem',
-                margin: '1rem auto',
                 width: '64rem',
+                margin: '1rem auto',
             }}
         >
             <Viewer
-                initialPage={5}
+                initialPage={20}
+                defaultScale={0.7}
                 scrollMode={ScrollMode.Page}
-                fileUrl={'/pdf-open-parameters.pdf'}
+                viewMode={ViewMode.DualPageWithCover}
+                fileUrl={'/100-pages-blank.pdf'}
                 plugins={[defaultLayoutPluginInstance]}
+                renderPage={renderPage}
             />
         </div>
     );
