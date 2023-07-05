10fc2d3af599f4a341c616054d761450ef234261
Phuoc Nguyen <me@phuoc.ng>
2023-07-05 12:39:43 +0700

    feat(demo): Stream documents

--- demo/nextjs/pages/api/doc/[...slug].ts
+++ demo/nextjs/pages/api/doc/[...slug].ts
@@ -2,14 +2,30 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs';
 import path from 'path';
 
+export const config = {
+    api: {
+        responseLimit: false,
+    },
+};
+
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
     const slug = (req.query.slug as string[]).join('');
     const documentPath = slug.endsWith('.pdf') ? slug : `${slug}.pdf`;
     const filePath = path.resolve('.', `../../samples/${documentPath}`);
 
-    res.setHeader('Content-Type', 'application/pdf');
-    const documentContent = fs.readFileSync(filePath);
-    return res.send(documentContent);
+    const fileSize = fs.statSync(filePath).size;
+    res.writeHead(200, {
+        'Accept-Ranges': 'bytes',
+        'Content-Length': fileSize,
+        'Content-Type': 'application/pdf',
+    });
+
+    const readStream = fs.createReadStream(filePath,
+        {
+            highWaterMark: 512,
+        }
+    );
+    readStream.pipe(res);
 }
 
 export default handler;
