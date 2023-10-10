b017ae847ed081212f3ab851e6f97606cf7a0e23
Isaac <91521821+isimisi@users.noreply.github.com>
2023-10-10 14:48:38 +0200

    Update index.d.ts

--- packages/search/src/index.d.ts
+++ packages/search/src/index.d.ts
@@ -35,6 +35,7 @@ export interface MatchPosition {
     // Each page may have multiple matches
     matchIndex: number;
     pageIndex: number;
+    title: string;
 }
 
 export interface OnHighlightKeyword {
