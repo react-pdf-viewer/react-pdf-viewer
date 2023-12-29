20a16acf4dd71a4d9b5e56a128e16a3d562502b0
Phuoc Nguyen <me@phuoc.ng>
2023-12-29 16:17:40 +0700

    ref: Remove Portal

--- packages/core/src/portal/Modal.tsx
+++ packages/core/src/portal/Modal.tsx
@@ -9,11 +9,14 @@
 'use client';
 
 import * as React from 'react';
+import { useToggle } from '../hooks/useToggle';
 import type { Toggle } from '../types/Toggle';
 import { uniqueId } from '../utils/uniqueId';
 import { ModalBody } from './ModalBody';
 import { ModalOverlay } from './ModalOverlay';
-import { Portal, RenderContent, RenderTarget } from './Portal';
+
+export type RenderContent = (toggle: Toggle) => React.ReactNode;
+export type RenderTarget = (toggle: Toggle, opened: boolean) => React.ReactNode;
 
 export const Modal: React.FC<{
     ariaControlsSuffix?: string;
@@ -24,6 +27,7 @@ export const Modal: React.FC<{
     target?: RenderTarget;
 }> = ({ ariaControlsSuffix, closeOnClickOutside, closeOnEscape, content, isOpened = false, target }) => {
     const controlsSuffix = ariaControlsSuffix || `${uniqueId()}`;
+    const { opened, toggle } = useToggle(isOpened);
 
     const renderTarget = (toggle: Toggle, opened: boolean): React.ReactElement => (
         <div
@@ -48,5 +52,10 @@ export const Modal: React.FC<{
         </ModalOverlay>
     );
 
-    return <Portal target={target ? renderTarget : null} content={renderContent} isOpened={isOpened} />;
+    return (
+        <>
+            {target && renderTarget(toggle, opened)}
+            {opened && renderContent(toggle)}
+        </>
+    );
 };
