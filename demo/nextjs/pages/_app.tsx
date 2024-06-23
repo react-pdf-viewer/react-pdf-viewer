1ca17f09e2340afe580013539f64ef199f4a8812
Phuoc Nguyen <me@phuoc.ng>
2024-06-23 21:41:53 +0700

    feat: Use new Provider

--- demo/nextjs/pages/_app.tsx
+++ demo/nextjs/pages/_app.tsx
@@ -1,15 +1,18 @@
+import { Provider, type PdfJsApiProvider } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import '@react-pdf-viewer/drop/lib/styles/index.css';
 import '@react-pdf-viewer/highlight/lib/styles/index.css';
-import { Worker } from '@react-pdf-viewer/core';
+import * as PdfJs from 'pdfjs-dist';
 import * as React from 'react';
 import '../styles/index.css';
 
 export default function MyApp({ Component, pageProps }) {
+    const apiProvider = PdfJs as unknown as PdfJsApiProvider;
+
     return (
-        <Worker workerUrl="/pdf.worker.min.mjs">
+        <Provider pdfApiProvider={apiProvider} workerUrl="/pdf.worker.min.mjs">
             <Component {...pageProps} />
-        </Worker>
+        </Provider>
     );
 }
