3cd9c551b61ea0f01646f8e11095b366e976be9a
Phuoc Nguyen <me@phuoc.ng>
2024-07-21 22:50:20 +0700

    feat: Shorten generated CSS classes

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -6,63 +6,76 @@ type PackageJson = {
 
 import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
+import { createGenerateScopedName } from 'hash-css-selector';
 import fs from 'node:fs';
 import path from 'node:path';
-import { rollup, type RollupOptions } from 'rollup';
+import { rollup, type OutputOptions, type RollupOptions } from 'rollup';
 import esbuild from 'rollup-plugin-esbuild';
 import postcss from 'rollup-plugin-postcss';
 
-const rootPackagePath = process.cwd();
-const input = path.join(rootPackagePath, 'src/index.ts');
-const packageContent = fs.readFileSync(path.join(rootPackagePath, 'package.json'), { encoding: 'utf8' });
-const packageJson = JSON.parse(packageContent) as unknown as PackageJson;
+const buildPackage = async (rootPackagePath: string) => {
+    const input = path.join(rootPackagePath, 'src/index.ts');
+    const packageContent = fs.readFileSync(path.join(rootPackagePath, 'package.json'), { encoding: 'utf8' });
+    const packageJson = JSON.parse(packageContent) as unknown as PackageJson;
 
-const outputDir = path.join(rootPackagePath, 'lib');
-const packageName = packageJson.name.split('/').pop();
+    const outputDir = path.join(rootPackagePath, 'lib');
+    const packageName = packageJson.name.split('/').pop();
 
-const external = [
-    ...Object.keys(packageJson.dependencies || {}),
-    ...Object.keys(packageJson.peerDependencies || {}),
-];
+    const external = [
+        ...Object.keys(packageJson.dependencies || {}),
+        ...Object.keys(packageJson.peerDependencies || {}),
+    ];
 
-const plugins = [
-    json(),
-    // Inject CSS modules
-    postcss({
-        //extract: true,
-        modules: true,
-    }),
-    esbuild({
-        sourceMap: false,
-        tsconfig: path.join(rootPackagePath, 'tsconfig.json'),
-    }),
-];
+    const plugins = [
+        json(),
+        esbuild({
+            sourceMap: false,
+            tsconfig: path.join(rootPackagePath, 'tsconfig.json'),
+        }),
+        // Inject CSS modules
+        postcss({
+            // extract: true,
+            modules: {
+                // By default, all CSS classes are prefixed with `m-`
+                generateScopedName: createGenerateScopedName('rpv'),
+            },
+        }),
+    ];
 
-const rollupOptions: RollupOptions = {
-    input,
-    output: [
+    const outputs: OutputOptions[] = [
         {
             exports: 'named',
             file: path.join(outputDir, `cjs/${packageName}.js`),
             format: 'cjs',
+            plugins,
         },
-        // {
-        //     exports: 'named',
-        //     file: path.join(outputDir, `cjs/${packageName}.min.js`),
-        //     format: 'cjs',
-        //     plugins: plugins.concat([terser()]),
-        // },
-    ],
-    external,
-    plugins,
-    onwarn: (warning, warn) => {
-        // Ignore the warning shown when the `use client` directive is used at the top of files
-        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-            return;
-        }
-        warn(warning);
-    },
+        {
+            exports: 'named',
+            file: path.join(outputDir, `cjs/${packageName}.min.js`),
+            format: 'cjs',
+            plugins: plugins.concat([terser()]),
+        },
+    ];
+
+    const rollupOptions: RollupOptions = {
+        input,
+        output: outputs,
+        external,
+        plugins,
+        onwarn: (warning, warn) => {
+            // Ignore the warning shown when the `use client` directive is used at the top of files
+            if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+                return;
+            }
+            warn(warning);
+        },
+    };
+
+    // Compile
+    const build = await rollup(rollupOptions);
+    return Promise.all(outputs.map((output) => build.write(output)));
 };
 
-// Compile
-rollup(rollupOptions);
+(async () => {
+    buildPackage(process.cwd());
+})();
