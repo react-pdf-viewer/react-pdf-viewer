4d73dc03be33a1e258bccca1703b88a196a5a8f3
Phuoc Nguyen <me@phuoc.ng>
2024-07-31 22:24:53 +0700

    feat: Scale out modal body when it's closed

--- packages/core/src/portal/ModalBody.tsx
+++ packages/core/src/portal/ModalBody.tsx
@@ -9,37 +9,128 @@
 'use client';
 
 import * as React from 'react';
-import { useIsomorphicLayoutEffect } from '../hooks/useIsomorphicLayoutEffect';
 import { useLockScroll } from '../hooks/useLockScroll';
 import styles from '../styles/modalBody.module.css';
 import { TextDirection, ThemeContext } from '../theme/ThemeContext';
+import type { Toggle } from '../types/Toggle';
 import { classNames } from '../utils/classNames';
 import { mergeRefs } from '../utils/mergeRefs';
 import { useClickOutsideStack } from './useClickOutsideStack';
 import { useEscapeStack } from './useEscapeStack';
 
+type RenderChildrenProps = ({ onClose }: { onClose: Toggle }) => React.ReactNode;
+
 export const ModalBody: React.FC<{
     ariaControlsSuffix: string;
-    children?: React.ReactNode;
+    children: RenderChildrenProps;
     closeOnClickOutside: boolean;
     closeOnEscape: boolean;
-    onClose(): void;
+    onClose: Toggle;
 }> = ({ ariaControlsSuffix, children, closeOnClickOutside, closeOnEscape, onClose }) => {
     const { direction } = React.useContext(ThemeContext);
     const isRtl = direction === TextDirection.RightToLeft;
 
+    const overlayRef = React.useRef<HTMLDivElement>(null);
     const contentRef = React.useRef<HTMLElement>();
-    const [contentCallbackRef] = useClickOutsideStack(closeOnClickOutside, onClose);
+
+    const animationOptions: KeyframeAnimationOptions = {
+        duration: 150,
+        fill: 'forwards',
+    };
+
+    const handleClose = () => {
+        const overlayEle = overlayRef.current;
+        const contentEle = contentRef.current;
+        if (!overlayEle || !contentEle) {
+            return;
+        }
+
+        // Scale out the body
+        contentEle.animate(
+            [
+                {
+                    opacity: 1,
+                    transform: 'scale(1)',
+                },
+                {
+                    opacity: 0,
+                    transform: 'scale(0.9)',
+                },
+            ],
+            animationOptions,
+        );
+
+        // Fade out the overlay
+        const overlayAnimation = overlayEle.animate(
+            [
+                {
+                    background: 'rgba(0, 0, 0, 0.8)',
+                    opacity: 1,
+                },
+                {
+                    background: 'rgba(0, 0, 0, 1)',
+                    opacity: 0,
+                },
+            ],
+            animationOptions,
+        );
+        overlayAnimation.finished.then(() => {
+            onClose();
+        });
+    };
+
+    const [contentCallbackRef] = useClickOutsideStack(closeOnClickOutside, handleClose);
     const mergedContentRef = mergeRefs([contentRef, contentCallbackRef]);
 
     useLockScroll();
     useEscapeStack(() => {
         if (closeOnEscape) {
-            onClose();
+            handleClose();
         }
     });
 
-    useIsomorphicLayoutEffect(() => {
+    React.useEffect(() => {
+        const overlayEle = overlayRef.current;
+        const contentEle = contentRef.current;
+        if (!overlayEle || !contentEle) {
+            return;
+        }
+        // Fade in the overlay
+        const overlayAnimation = overlayEle.animate(
+            [
+                {
+                    background: 'rgba(0, 0, 0, 1)',
+                    opacity: 0,
+                },
+                {
+                    background: 'rgba(0, 0, 0, 0.8)',
+                    opacity: 1,
+                },
+            ],
+            animationOptions,
+        );
+        // Scale the modal content
+        const contentAnimation = contentEle.animate(
+            [
+                {
+                    opacity: 0,
+                    transform: 'scale(0.9)',
+                },
+                {
+                    opacity: 1,
+                    transform: 'scale(1)',
+                },
+            ],
+            animationOptions,
+        );
+
+        return () => {
+            overlayAnimation.cancel();
+            contentAnimation.cancel();
+        };
+    }, []);
+
+    React.useEffect(() => {
         const contentEle = contentRef.current;
         if (!contentEle) {
             return;
@@ -51,42 +142,23 @@ export const ModalBody: React.FC<{
             contentEle.style.overflow = 'auto';
             contentEle.style.maxHeight = `${maxHeight}px`;
         }
-
-        // Scale the modal content
-        const animation = contentEle.animate(
-            [
-                {
-                    opacity: 0,
-                    transform: 'scale(0.9)',
-                },
-                {
-                    opacity: 1,
-                    transform: 'scale(1)',
-                },
-            ],
-            {
-                duration: 150,
-                fill: 'forwards',
-            }
-        );
-        return () => {
-            animation.cancel();
-        };
     }, []);
 
     return (
-        <div
-            aria-modal="true"
-            className={classNames({
-                [styles.body]: true,
-                [styles.bodyRtl]: isRtl,
-            })}
-            id={`rpv-core__modal-body-${ariaControlsSuffix}`}
-            ref={mergedContentRef}
-            role="dialog"
-            tabIndex={-1}
-        >
-            {children}
+        <div className={styles.overlay} ref={overlayRef}>
+            <div
+                aria-modal="true"
+                className={classNames({
+                    [styles.body]: true,
+                    [styles.bodyRtl]: isRtl,
+                })}
+                id={`rpv-core__modal-body-${ariaControlsSuffix}`}
+                ref={mergedContentRef}
+                role="dialog"
+                tabIndex={-1}
+            >
+                {children({ onClose: handleClose })}
+            </div>
         </div>
     );
 };
