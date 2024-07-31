abedb661360a2558040eab9db77d2b0e4fe99e82
Phuoc Nguyen <me@phuoc.ng>
2024-07-31 22:00:02 +0700

    feat: Fade in the modal overlay

--- packages/core/src/portal/ModalOverlay.tsx
+++ packages/core/src/portal/ModalOverlay.tsx
@@ -11,4 +11,39 @@ import styles from '../styles/modalOverlay.module.css';
 
 export const ModalOverlay: React.FC<{
     children?: React.ReactNode;
-}> = ({ children }) => <div className={styles.overlay}>{children}</div>;
+}> = ({ children }) => {
+    const ref = React.useRef<HTMLDivElement>(null);
+
+    React.useEffect(() => {
+        const ele = ref.current;
+        if (!ele) {
+            return;
+        }
+        // Fade in the overlay
+        const animation = ele.animate(
+            [
+                {
+                    background: 'rgba(0, 0, 0, 1)',
+                    opacity: 0,
+                },
+                {
+                    background: 'rgba(0, 0, 0, 0.8)',
+                    opacity: 1,
+                },
+            ],
+            {
+                duration: 150,
+                fill: 'forwards',
+            }
+        );
+        return () => {
+            animation.cancel();
+        };
+    }, []);
+
+    return (
+        <div className={styles.overlay} ref={ref}>
+            {children}
+        </div>
+    );
+};
